dedf4dc780ad78b04b38325d5fea83164a12b5a4
Rlulu <64244191+strangeriam@users.noreply.github.com>
2021-03-11 10:40:21 +0800

    Update fileevent - rs232 .tcl

--- book-advance/fileevent - rs232 .tcl
+++ book-advance/fileevent - rs232 .tcl
@@ -1,32 +1,26 @@
+proc getline {chan} {
+        gets $chan line
+
+        if { ![eof $chan] } {
+                puts "$line"
+        } else {
+                close $chan
+        }
+}
+
 proc com_try {} {
       global com_fd
 
-      if { ![catch { set com_fd [open "COM1:" r+] } result] } {
-
-               puts "Get Console connection --> COM1."
-               fconfigure $com_fd -blocking 0 -buffering line -mode 115200,n,8,1 -translation cr
+      if { ![catch { set com_fd [open "com1:" r+] } result] } {
+               fileevent $com_fd readable [list getline $com_fd]
+               fconfigure $com_fd -blocking 0 -buffering line -mode 115200,n,8,1
                return $com_fd
       }
 }
 
-proc getline {chan} {
-        global done
+com_try
 
-        gets $chan line
-
-        if { ! [eof $chan] } {
-                puts ">> $line"
-        } else {
-                close $chan
-                set done 1
-        }
-}
-
-fileevent $com_fd readable [list getline $com_fd]
-fconfigure $com_fd -buffering line
-
-ConsoleWrite $com_fd ls
+ConsoleWrite $com_fd "brctl show"
 vwait_seconds 1
-#set aaa [ConsoleRead $com_fd]
 
-puts "aaa ==>$aaa"
+f_com_close $com_fd
