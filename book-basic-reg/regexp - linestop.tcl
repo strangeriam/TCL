05effa3c6540a1f86c993d60e9c4415d3f7065ba
Rlulu <64244191+strangeriam@users.noreply.github.com>
2021-08-02 10:55:15 +0800

    Update regexp - linestop.tcl

--- book-basic-reg/regexp - linestop.tcl
+++ book-basic-reg/regexp - linestop.tcl
@@ -1,9 +1,11 @@
+
+regexp -linestop {condition.*} $source newVar ;# 取條件後面字串.
+regexp -linestop {.*condition} $source newVar ;# 取條件前面字串.
+
 ## This example.
 set readOS_tmp {
 	[root@master-0 vesbkp]# cat /run/user/2205/aaa.tmp
-
 	Operating System: CentOS Linux 7.2006.9 (Core)
-
 	[root@master-0 vesbkp]#
 }
 
@@ -12,3 +14,6 @@ regexp -linestop {Operating System.*} $readOS_tmp readOS
 puts "==>$readOS<=="
 
 ## OUTPUT: ==>Operating System: CentOS Linux 7.2006.9 (Core)<==
+
+
+
