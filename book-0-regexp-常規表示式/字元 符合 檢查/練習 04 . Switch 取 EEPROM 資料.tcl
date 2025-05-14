93f5260af2063865b795d5d5d638f3c476b50999
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-14 16:16:04 +0800

    Update 練習 04 . Switch 取 EEPROM 資料.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/練習 04 . Switch 取 EEPROM 資料.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習 04 . Switch 取 EEPROM 資料.tcl
@@ -1,4 +1,56 @@
 
+proc _f_ReadFile { fname } {
+	if { [file exists $fname] } {
+		set fd [open $fname r]
+		set data [read $fd]
+		close $fd
+		return $data
+	} else {
+		return ""
+	}
+}
+
+cd D:/Dropbox/12-Office-Sync-MTS/36_ECS4650/Others/ethspf_fail_check
+set infile [_f_ReadFile Fail2.txt]
+
+set HWInfo [list pid bid hwver serialno macadr mdate modelno]
+set HWval [list $::HW_pid \
+					$::HW_bid \
+					$::HW_hwver \
+					[_f_vini_profilerd SFIS SN] \
+					[Output_Mac_convstr_JmpNo [_f_vini_profilerd SFIS MAC] : 0] \
+					[clock format [expr [clock second] + 2 ] -format "%Y-%m-%d"] \
+					$::HW_ID]
+
+if { [info exists faillist] } {unset faillist}
+
+set HWitem [list "Project ID" \
+					"Board ID" \
+					"Hardware Version" \
+					"Serial Number" \
+					"Mac Address" \
+					"Manufacture Date" \
+					"Model Number" ]
+
+foreach item $HWitem val $HWval {
+		if { [ regexp ID $item ] } {
+			set regline "${item}\\s+=0x[format "%08x" $val]"
+		} else {
+			if { [ regexp Date $item ] } {
+				set regline "${item}\\s+=\\d+-\\d+-\\d+"
+			} else {
+				set regline "${item}\\s+=$val"
+			}
+    }
+
+		if { ![regexp -line $regline $infile] } {
+			lappend faillist "${item}:${val}"
+		}
+	}
+
+	if { [info exists faillist] } { puts "$faillist Fail" }
+
+
 
 
 # Log
