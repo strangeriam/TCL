7f6ca354acc7386c3d79fea6a5f67869765ad450
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-14 15:38:13 +0800

    Create 練習 04 . Switch 取 EEPROM 資料.tcl

---
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習 04 . Switch 取 EEPROM 資料.tcl
@@ -0,0 +1,20 @@
+
+
+
+# Log
+## =====================================
+Marvell>> printsysinfo
+UC_MGR_GetSysInfo 00000002ffe00000 ok
+----------------------------------------
+Project ID           =0x0000020d
+Board ID             =0x00000001
+Bootloader Version   =0.0.2.0
+Hardware Version     =R01
+Serial Number        =EC2430001796
+Mac Address          =7C:8D:9C:4D:9E:20
+Manufacture Date     =2025-02-17
+Model Number         =ECS4650-54P
+Service Tag          =CONFIG_SERVICE_TAG
+----------------------------------------
+Marvell>> <INTERRUPT>
+Marvell>> 
