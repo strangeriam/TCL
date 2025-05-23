7ae35220f8edb5dad7dad6f8886b37ca3477aed4
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-23 13:36:39 +0800

    Update 02 . RS232.tcl

--- TK/指令輸出至另一視窗/02 . RS232.tcl
+++ TK/指令輸出至另一視窗/02 . RS232.tcl
@@ -27,6 +27,22 @@ proc termread { comportid } {
 proc termout {key keycode} {
 }
 
+proc com_open { port } {
+	set msgTitle "Check RS232 Console !!!"
+	set msgEnglish "\b\b\bStep 1: Disconnect TeraTerm or SecureCRT.\n\b\b\bStep 2: Click OK."
+
+	for {set i 1} { $i <= 3 } {incr i} {
+		if { [catch { set fd [open \\\\.\\COM$port r+] } result] } {
+			tk_messageBox -type ok -message "$msgTitle\n\n\n$msgEnglish" -icon warning -detail ""
+		} else {
+			fconfigure $fd -blocking 0 -buffering line -mode 115200,n,8,1 -translation cr
+			break
+		}
+	}
+
+	return $fd
+}
+
 proc com_close { consoleid } {
 	catch {close $consoleid}
 	set consoleid 0
