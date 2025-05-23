149624359832c7b516e6412e525b147ed4a44c13
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-23 08:35:14 +0800

    Update 001.tcl

--- TK/指令輸出至另一視窗/001.tcl
+++ TK/指令輸出至另一視窗/001.tcl
@@ -1,32 +1,31 @@
 
 proc UI_ShowLog {} {
-        global w
+	global w
 
-    set w .gui[incr counter]
-    toplevel $w
-    wm title $w "Lu's new project."
+	set w .gui[incr counter]
+	toplevel $w
+	wm title $w "Lu's new project."
 
-        text $w.t -yscrollcommand [list .scrolly set] -setgrid 0 -height 40 -maxundo 3 -autoseparators 0 -undo false -bg blue4 -fg white -autosep 1
-        ::ttk::scrollbar .scrolly -orient vertical -command [list $w.t yview]
+	text $w.t -yscrollcommand [list .scrolly set] -setgrid 0 -height 40 -maxundo 3 -autoseparators 0 -undo false -bg blue4 -fg white -autosep 1
+	::ttk::scrollbar .scrolly -orient vertical -command [list $w.t yview]
 
-        pack .scrolly -side right -fill y
-        pack $w.t -side left -fill both -expand 1
-        bind $w.t <Any-Key> [list _f_ShowLog_termout %A %k]
+	pack .scrolly -side right -fill y
+	pack $w.t -side left -fill both -expand 1
+	bind $w.t <Any-Key> [list _f_ShowLog_termout %A %k]
 }
 
 proc termread { comportid } {
-        global w
+	global w
 
-    catch {set resp [read $comportid]}
-    if { [info exists resp] } {
-        $w.t insert end $resp
-        $w.t see end
-    }
+	catch {set resp [read $comportid]}
+	if { [info exists resp] } {
+		$w.t insert end $resp
+		$w.t see end
+	}
 }
 
 proc termout {key keycode} {
 }
-
 
 package require Tk
 
@@ -35,7 +34,6 @@ UI_ShowLog
 
 set ::fd [open "|cmd.exe" r+]
 fconfigure $::fd -blocking 0 -buffering line
-
 
 puts $::fd dir
 termread $::fd
