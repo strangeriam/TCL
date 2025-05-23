06d0ccbe37a9871140783ba5ca940f20b26036c1
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-23 13:32:11 +0800

    Create 02 . RS232.tcl

---
+++ TK/指令輸出至另一視窗/02 . RS232.tcl
@@ -0,0 +1,43 @@
+
+proc UI_ShowLog {} {
+	global w
+
+	set w .gui[incr counter]
+	toplevel $w
+	wm title $w "Lu's new project."
+
+	text $w.t -yscrollcommand [list .scrolly set] -setgrid 0 -height 40 -maxundo 3 -autoseparators 0 -undo false -bg blue4 -fg white -autosep 1
+	::ttk::scrollbar .scrolly -orient vertical -command [list $w.t yview]
+
+	pack .scrolly -side right -fill y
+	pack $w.t -side left -fill both -expand 1
+	bind $w.t <Any-Key> [list _f_ShowLog_termout %A %k]
+}
+
+proc termread { comportid } {
+	global w
+
+	catch {set resp [read $comportid]}
+	if { [info exists resp] } {
+		$w.t insert end $resp
+		$w.t see end
+	}
+}
+
+proc termout {key keycode} {
+}
+
+package require Tk
+
+set counter 0
+UI_ShowLog
+
+set ::fd [open "|cmd.exe" r+]
+fconfigure $::fd -blocking 0 -buffering line
+
+puts $::fd dir
+termread $::fd
+
+
+;# 移除 此 uI
+destroy $w
