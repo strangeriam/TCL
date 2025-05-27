a81a63053dea7f7160b16ad6e042fa5f7cf4982e
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-27 10:13:19 +0800

    Create 穿插時間中斷 2.tcl

---
+++ book-thread/example2/穿插時間中斷 2.tcl
@@ -0,0 +1,39 @@
+proc vwait_mseconds {msec} {
+	 set vwait_flag 0
+	 after [expr $msec] { set vwait_flag 1 }
+	 vwait vwait_flag
+}
+
+proc f_say1 {} {
+    for {set i 1} {$i <= 5} {incr i} {
+        puts "say1 . $i"
+	vwait_mseconds 1000
+    }
+}
+
+proc f_say2 {} {
+    for {set i 1} {$i <= 8} {incr i} {
+        puts "say2 . $i"
+	vwait_mseconds 500
+    }
+}
+
+package require Thread
+tsv::set share mainTid [thread::id]
+
+set tid1 [thread::create -joinable {
+            set mainTid [tsv::get share mainTid]
+            thread::send -async $mainTid [list f_say1]
+}]
+
+set tid2 [thread::create -joinable {
+            set mainTid [tsv::get share mainTid]
+            thread::send -async $mainTid [list f_say2]
+}]
+
+puts "wait 5 seconds .. start"
+vwait_mseconds 5000
+puts "wait 5 seconds .. end"
+
+thread::join $tid1
+thread::join $tid2
