e244ce9580ddc45f679f6495815e4a02acd3e381
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-27 15:54:18 +0800

    Update 穿插時間中斷 2.tcl

--- book-thread/example2/穿插時間中斷 2.tcl
+++ book-thread/example2/穿插時間中斷 2.tcl
@@ -1,53 +1,82 @@
+最近
+how to program tcl thread
+$25
+AI Model:default
+how to program tcl thread
+答案
+Tcl (Tool Command Language) is a high-level, general-purpose, interpreted, scripting language. Tcl threads are used for concurrent programming in Tcl. Here's a basic example of how to use threads in Tcl:
+
+Tcl Thread Basics
+
+To use threads in Tcl, you need to load thethreadpackage. You can do this using the following command:
+
+tcl
+package require Thread
+Creating a Thread
+
+To create a thread, you can use thethread::createcommand. This command takes a script as an argument, which will be executed in the new thread.
+
+tcl
 package require Thread
 
-proc vwait_mseconds {msec} {
-	 set vwait_flag 0
-	 after [expr $msec] { set vwait_flag 1 }
-	 vwait vwait_flag
-}
+set thread_id [thread::create {
+    # Code to be executed in the new thread
+    puts "Hello from new thread!"
+}]
+Communicating with a Thread
 
-proc f_say1 {} {
-    for {set i 1} {$i <= 5} {incr i} {
-        puts "say1 . $i"
-	vwait_mseconds 1000
-    }
-}
+You can use thethread::sendcommand to send a command to a thread for execution. Thethread::sendcommand takes the thread ID and the command to be executed as arguments.
 
-proc f_say2 {} {
-    for {set i 1} {$i <= 8} {incr i} {
-        puts "say2 . $i"
-	vwait_mseconds 500
-    }
-}
+tcl
+package require Thread
 
-unset ::sstop
-
-tsv::set share mainTid [thread::id]
-
-set tid1 [thread::create -joinable {
-            set mainTid [tsv::get share mainTid]
-            thread::send -async $mainTid [list f_say1]
+set thread_id [thread::create {
+    # Code to be executed in the new thread
+    puts "Hello from new thread!"
 }]
 
-f_say2
+# Send a command to the thread
+thread::send $thread_id {
+    puts "Received command from main thread!"
+}
+Waiting for a Thread to Finish
 
-puts "wait 20 seconds .. start"
-vwait_mseconds 20000
-puts "wait 20 seconds .. end"
+You can use thethread::waitcommand to wait for a thread to finish execution. Thethread::waitcommand takes the thread ID as an argument.
 
-thread::join $tid1
+tcl
+package require Thread
 
-;# 輸出
-say2 . 1
-say1 . 1
-say1 . 2
-say1 . 3
-say1 . 4
-say1 . 5
-say2 . 2
-say2 . 3
-say2 . 4
-say2 . 5
-say2 . 6
-say2 . 7
-say2 . 8
+set thread_id [thread::create {
+    # Code to be executed in the new thread
+    puts "Hello from new thread!"
+}]
+
+# Wait for the thread to finish
+thread::wait $thread_id
+Example Use Case
+
+Here's an example use case that demonstrates the use of Tcl threads:
+
+tcl
+package require Thread
+
+# Function to be executed in a new thread
+proc worker {thread_id} {
+    puts "Worker $thread_id started"
+    for {set i 0} {$i < 5} {incr i} {
+        puts "Worker $thread_id: $i"
+        after 1000
+    }
+    puts "Worker $thread_id finished"
+}
+
+# Create two threads
+set thread_id1 [thread::create [list worker 1]]
+set thread_id2 [thread::create [list worker 2]]
+
+# Wait for both threads to finish
+thread::wait $thread_id1
+thread::wait $thread_id2
+In this example, two threads are created, each executing theworkerprocedure. Theworkerprocedure simulates some work by printing numbers from 0 to 4 with a delay of 1 second between each number. The main thread waits for both worker threads to finish using thethread::waitcommand.
+
+Enter search terms...
