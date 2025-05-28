26466e8593c1a003feea9efb3b808a85bee6fc8f
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-28 15:46:44 +0800

    Update debugging.tcl

--- book-thread/example2/debugging.tcl
+++ book-thread/example2/debugging.tcl
@@ -1,16 +1,65 @@
 package require Thread
-catch {console show}
-set ::gThread [thread::create {thread::wait}]
-puts [list {created thread} $::gThread]
-proc test {} {
-    puts {test starting}
-    thread::send -async $::gThread {puts [clock seconds]}
-    after 2000 test
-    puts {test ending}
+
+# catch {console show}
+
+set ::tid1 [thread::create {thread::wait}]
+puts [list {created thread 1} $::tid1]
+
+set ::tid2 [thread::create {thread::wait}]
+puts [list {created thread 2} $::tid2]
+
+proc test1 {} {
+    puts {test 1 . starting}
+    thread::send -async $::tid1 {puts "test 1 . [clock seconds]"}
+    after 2000 test1
+    puts {test 1 . ending}
 }
 
-test
-puts {started first test}
+proc test2 {} {
+    puts {test 2 . starting}
+    thread::send -async $::tid2 {puts "test 2 . [clock seconds]"}
+    after 2000 test2
+    puts {test 2 . ending}
+}
+
+
+test1
+test2
 
 #only needed for tclsh, to keep the interpreter alive and keep the event loop running
 vwait forever
+
+
+;# 輸出:
+% test1
+test 1 . starting
+test 1 . ending
+test 1 . 1748418313
+% test2
+test 2 . starting
+test 2 . ending
+test 2 . 1748418313
+test 1 . starting
+test 1 . ending
+test 1 . 1748418315
+test 2 . starting
+test 2 . 1748418315
+test 2 . ending
+test 1 . starting
+test 1 . ending
+test 1 . 1748418317
+test 2 . starting
+test 2 . ending
+test 2 . 1748418317
+test 1 . starting
+test 1 . ending
+test 1 . 1748418319
+test 2 . starting
+test 2 . ending
+test 2 . 1748418319
+test 1 . starting
+test 1 . ending
+test 1 . 1748418321
+test 2 . starting
+...
+
