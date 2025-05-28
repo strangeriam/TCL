4397851900536a34210721acbe6f3a2782cdbf05
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-28 17:04:21 +0800

    Update 03.tcl

--- udemy . TCL Programming/Chapter 3 . Advanced TCL/92 . Multi-thread/03.tcl
+++ udemy . TCL Programming/Chapter 3 . Advanced TCL/92 . Multi-thread/03.tcl
@@ -3,14 +3,14 @@ package require Thread
 set mutex [thread::mutex]
 set condition [thread::contition]
 
-set thread1 [thread::create{
+set thread1 [thread::create {
     thread::mutex lock $mutex
     thread::condition wait $condition $mutex
     puts "Thread 1 received signal"
     thread::mutex unlock $mutex
 }]
 
-set thread2 [thread::create 
+set thread2 [thread::create {
     thread::after 2000
     thread::mutex lock $mutex
     thread::condition signal $condition
