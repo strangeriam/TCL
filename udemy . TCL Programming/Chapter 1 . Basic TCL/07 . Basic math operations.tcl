6007fb05c6ab9e41f271ad7fe04e132468fb5824
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-06-05 09:24:43 +0800

    Create 07 . Basic math operations.tcl

---
+++ udemy . TCL Programming/Chapter 1 . Basic TCL/07 . Basic math operations.tcl
@@ -0,0 +1,11 @@
+X + Y . Adds X and Y ----------------------------> expr 10 + 0.001
+X - Y . Substracts Y from X ---------------------> expr 9 - 24
+X * Y . Multiplies X times Y --------------------> expr 3 * 3.14
+X / Y . Divides X into Y ------------------------> expr 100 / 2
+X % Y . Modulus between X and Y -----------------> expr 100 / 3
+
+sqrt(Y) . Square root of X ----------------------> expr sqrt(100)
+abs(Y) . Absolute value of X --------------------> expr abs(-15)
+pow(X,Y) . X raised to Y power ------------------> expr pow(2,3)
+rand() . Pseudo-random number between 0 and 1 ---> expr rand()
+round(X) . Round X, if that is not an integer ---> expr round(3.1)
