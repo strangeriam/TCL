3d5e60f94ad8af180ffd2fab78fcf94f611df243
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-06-06 09:28:46 +0800

    Update 15 . Strings . excape sequences.tcl

--- udemy . TCL Programming/Chapter 1 . Basic TCL/15 . Strings . excape sequences.tcl
+++ udemy . TCL Programming/Chapter 1 . Basic TCL/15 . Strings . excape sequences.tcl
@@ -1,8 +1,13 @@
-Escape sequence / Description
-\n . New line
-\r . Carriage return
-\t . Horizontal tab
-\v . Vertical tab
-\a . Alert or bell (audible)
-\b . Backspace
-\f . From feed (page break)
+Escape sequence
+        / Description             / result
+\n      . New line                . puts "Hello\nWorld"
+                                  輸出: Hello
+                                        World
+
+\r      . Carriage return         . 結果與 \n 一樣
+\t      . Horizontal tab          . "Hello   World" 像是兩字之間按了 tab 健一樣的空白.
+\v      . Vertical tab            . "Hello^KWorld" 多了 ^K
+\a      . Alert or bell (audible) . "發出聲音"
+\b      . Backspace               . "HellWorld" 消除 \b 的前一個字母, Hello 的 o 不見了.
+\f      . From feed (page break)  . 沒變
+
