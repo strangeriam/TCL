f651d5e567aea0a55f1816e20ae23d13a8c998ba
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-06-10 14:26:26 +0800

    Update 15b . Basic string commands.tcl

--- udemy . TCL Programming/Chapter 1 . Basic TCL/15b . Basic string commands.tcl
+++ udemy . TCL Programming/Chapter 1 . Basic TCL/15b . Basic string commands.tcl
@@ -5,3 +5,18 @@ string last STR1 STR2
 string length STR
 string ratch PATTERN STR
 string range STR n1 n2
+
+
+
+rather --> TCL 8.5 not support.
+set p "AAA"
+set str "this is my AAA not yours."
+
+TCL wronging message.
+string ratch $p $str
+unknown or ambiguous subcommand "ratch": must be 
+bytelength, compare, equal, first, index, is, last, length, map, 
+match, range, repeat, replace, reverse, tolower, totitle, toupper, 
+trim, trimleft, trimright, wordend, or wordstart
+
+
