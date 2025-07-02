d128c68d990412cf6a2ae00d4f63a99987bcc4d6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-07-02 08:42:50 +0800

    Update and rename regexp to regexp.tcl

---
+++ book-0-regexp-常規表示式/regexp.tcl
@@ -0,0 +1,24 @@
+# 字串比對
+;# Note-250700: 內容有含字串, 則回傳 1. 反之 0.
+;# Note-260702: 比對內容可使用變數或字串.
+;# Note-260702: 比對內容區分大小寫.
+
+set content "The result is PASS"
+
+;# Example 1
+regexp {PASS} $content
+;# 輸出: 1
+
+;# Example 2
+regexp {Pass} $content
+;# 輸出: 0
+
+;# Example 3
+set var PASS
+regexp $var $content
+;# 輸出: 1
+
+;# Example 4
+set var Pass
+;# 輸出: 0
+
