90ced77de5f24707d3b7cc2c6480113a2b751e23
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-07-02 09:36:58 +0800

    Update example . 含運算符號.tcl

--- book-0-regexp-常規表示式/regexp/example . 含運算符號.tcl
+++ book-0-regexp-常規表示式/regexp/example . 含運算符號.tcl
@@ -1,36 +1,36 @@
 
-set content {
+set get_info {
 Console#show license
 Currently Used Profile:
   L2+, cloud-m
 Console#
 }
 
-regexp {L2+, cloud-m} $content
+regexp {L2+, cloud-m} $get_info
 ;# 輸出: 0
 
-regexp {L2\+, cloud\-m} $content
+regexp {L2\+, cloud\-m} $get_info
 ;# 輸出: 1
 
 ;# =================================
-set var "L2+, cloud-m"
-regexp $var $content
+set ::Lic_profile "L2+, cloud-m"
+regexp $::Lic_profile $get_info
 ;# 輸出: 0
 
-set var "L2+, cloud-m"
-regexp ${var} $content
+set ::Lic_profile "L2+, cloud-m"
+regexp ${::Lic_profile} $get_info
 ;# 輸出: 0
 
-set var {L2+, cloud-m}
-regexp ${var} $content
+set ::Lic_profile {L2+, cloud-m}
+regexp ${::Lic_profile} $get_info
 ;# 輸出: 0
 
-set var "L2\+, cloud\-m"
-regexp $var $content
+set ::Lic_profile "L2\+, cloud\-m"
+regexp $::Lic_profile $get_info
 ;# 輸出: 0
 
-set var {L2\+, cloud\-m}
-regexp $var $content
+set ::Lic_profile {L2\+, cloud\-m}
+regexp $::Lic_profile $get_info
 ;# 輸出: 1
 
 
