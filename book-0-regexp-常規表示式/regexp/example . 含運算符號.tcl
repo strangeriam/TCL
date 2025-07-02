948be289d9169229eac4af80ad30f176d42b0dab
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-07-02 09:26:52 +0800

    Update example . 含運算符號.tcl

--- book-0-regexp-常規表示式/regexp/example . 含運算符號.tcl
+++ book-0-regexp-常規表示式/regexp/example . 含運算符號.tcl
@@ -1,13 +1,13 @@
 
-set aa {
+set content {
 Console#show license
 Currently Used Profile:
   L2+, cloud-m
 Console#
 }
 
-regexp {L2+, cloud-m} $aa
+regexp {L2+, cloud-m} $content
 ;# 輸出: 0
 
-regexp {L2\+, cloud\-m} $aa
+regexp {L2\+, cloud\-m} $content
 ;# 輸出: 1
