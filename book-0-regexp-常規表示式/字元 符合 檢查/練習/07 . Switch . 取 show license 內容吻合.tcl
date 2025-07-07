64039f8a9271f62a6b50c27905f34ec6d2c6b74b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-07-07 13:59:24 +0800

    Update 07 . Switch . 取 show license 內容吻合.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/練習/07 . Switch . 取 show license 內容吻合.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習/07 . Switch . 取 show license 內容吻合.tcl
@@ -1,8 +1,14 @@
 
-
 set get_info {
 Console#show license
 Currently Used Profile:
   L2+, cloud-m
 Console#
+}
+
+set lic 		"L2\\+, cloud-m"
+set lic_digest 	"d706d2cb898282347a8ce9589505c2f9"
+
+if { [regexp -line $lic $get_info] } {
+	return 0
 }
