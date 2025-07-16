23026c4cc412bace35ff8ef52117a6a7c14e9a31
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-07-16 10:40:20 +0800

    Update code.tcl

--- Training/HomeWork_005. Switch PoE 取 Fail Port/code.tcl
+++ Training/HomeWork_005. Switch PoE 取 Fail Port/code.tcl
@@ -11,3 +11,20 @@ proc PoE_Test_analyze_250715 {} {
 	if {![info exists faillist]} { set faillist "" }
 	return [lsort -integer $faillist]
 }
+
+set totalS 3
+for {set i 1} {$i <= $totalS} {incr i} {
+	set faillist [PoE_Test_analyze_250715]
+	if { [llength $faillist] == 0 } { break }
+
+	set totalFail [llength $faillist]
+	_f_termmsg_V1 "faillist \($totalFail\) . Try ${i}/$totalS\n$faillist"
+
+	if {$i == $totalS} {
+		_f_Client_SelectUI "CALL PE/TE for NG ports \($totalFail\) . ${i}/$totalS\n\b\b$faillist" FailOnly "./bitmap/parts/ECS4150-28P_ETH.png"
+		set ::s0 "PoE Fail" ; set ::ErrorCode "PoE"
+		return 0
+	}
+
+	_f_Client_SelectUI "NG ports list \($totalFail\) . Try ${i}/$totalS\n\b\b$faillist" PassOnly "./bitmap/parts/ECS4150-28P_ETH.png" "TXT:RETRY"
+  }
