483d8db84c85e1d64f99f204f88fb7a3fdc54943
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-08-13 15:45:47 +0800

    Create 08 . 取 uboot writing.tcl

---
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習/08 . 取 uboot writing.tcl
@@ -0,0 +1,26 @@
+
+
+
+
+set ::content {
+IPQ5018# 
+IPQ5018# imgaddr=$fileaddr && source $imgaddr:script
+## Executing script at 44000000
+crc32+ SPI_ADDR_LEN=3
+SF: Detected W25Q128FW with page size 256 Bytes, erase size 4 KiB, total 16 MiB
+soc_hw_version : Validation success
+machid : Validation success
+Flashing sbl1:                          [ done ]
+Flashing mibib:                         [ done ]
+Flashing bootconfig:                    [ done ]
+Flashing bootconfig1:                   [ done ]
+Flashing tz:                            [ done ]
+Flashing devcfg:                        [ done ]
+Flashing ddr-AP-MP03.5-C1_512M16_DDR3:  [ done ]
+Flashing appsblenv:                     [ done ]
+Flashing u-boot:                        [ done ]
+Flashing priv_data1:                    [ done ]
+Flashing ubi:                           [ done ]
+Flashing wifi_fw_ipq5018_qcn6122cs:     [ done ]
+IPQ5018# 
+}
