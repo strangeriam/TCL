af292c1f027172d843134ee655178efa1a6d2588
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-08-14 10:44:45 +0800

    Update 09 . uBoot writing.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/練習/09 . uBoot writing.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習/09 . uBoot writing.tcl
@@ -25,15 +25,19 @@ set reglist [list 	sbl1 \
 					ubi \
 					wifi_fw_ipq5018_qcn6122cs ]
 
-foreach item $reglist {
-	if { [regexp -linestop ${item}.* $get_info tmp] } {
+if { [info exists faillist] } { unset faillist }
+
+foreach item $itemlist val $reglist {
+	if { [regexp -linestop "Flashing $val\:.*" $get_info tmp] } {
 		if { ! [regexp {done} $tmp] } {
-	 		puts "FAIL: $item"
-			return 0
-	 	}
+			append faillist "$item "
+		}
 	}
 }
 
+if { [info exists faillist] } {
+	_f_termmsg_V1 "faillist: $faillist"
+}
 
 set get_info {
 IPQ5018# 
@@ -47,12 +51,12 @@ Flashing sbl1:                          [ done ]
 Flashing mibib:                         [ done ]
 Flashing bootconfig:                    [ done ]
 Flashing bootconfig1:                   [ done ]
-Flashing tz:                            [ done ]
+Flashing tz:                            [  ]
 Flashing devcfg:                        [ done ]
-Flashing ddr-AP-MP03.5-C1_512M16_DDR3:  [ done ]
+Flashing ddr-AP-MP03.5-C1_512M16_DDR3:  [ do ]
 Flashing appsblenv:                     [ done ]
-Flashing u-boot:                        [ done ]
-Flashing priv_data1:                    [ done ]
+Flashing u-boot:                        [ xdone ]
+Flashing priv_data1:                    [ dox ]
 Flashing ubi:                           [ done ]
 Flashing wifi_fw_ipq5018_qcn6122cs:     [ done ]
 IPQ5018# 
