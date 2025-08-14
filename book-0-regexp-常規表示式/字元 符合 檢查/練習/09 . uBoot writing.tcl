6bb9d6066d7b3b991e36c2bb51ba3f8bf8239cb6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-08-14 10:49:02 +0800

    Update 09 . uBoot writing.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/練習/09 . uBoot writing.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習/09 . uBoot writing.tcl
@@ -1,4 +1,29 @@
+;# LOG
+set get_info {
+IPQ5018# 
+IPQ5018# imgaddr=$fileaddr && source $imgaddr:script
+## Executing script at 44000000
+crc32+ SPI_ADDR_LEN=3
+SF: Detected W25Q128FW with page size 256 Bytes, erase size 4 KiB, total 16 MiB
+soc_hw_version : Validation success
+machid : Validation success
+Flashing sbl1:                          [ done ]
+Flashing mibib:                         [ done ]
+Flashing bootconfig:                    [ done ]
+Flashing bootconfig1:                   [ done ]
+Flashing tz:                            [  ]
+Flashing devcfg:                        [ done ]
+Flashing ddr-AP-MP03.5-C1_512M16_DDR3:  [ do ]
+Flashing appsblenv:                     [ done ]
+Flashing u-boot:                        [ dxone ]
+Flashing priv_data1:                    [ dox ]
+Flashing ubi:                           [ done ]
+Flashing wifi_fw_ipq5018_qcn6122cs:     [ done ]
+IPQ5018# 
+}
 
+# CODE Here
+# ======================================================
 set itemlist [list 	01_sbl1 \
 					02_mibib \
 					03_bootconfig \
@@ -37,27 +62,10 @@ foreach item $itemlist val $reglist {
 
 if { [info exists faillist] } {
 	puts "faillist: $faillist"
+ 	return 0
 }
 
-set get_info {
-IPQ5018# 
-IPQ5018# imgaddr=$fileaddr && source $imgaddr:script
-## Executing script at 44000000
-crc32+ SPI_ADDR_LEN=3
-SF: Detected W25Q128FW with page size 256 Bytes, erase size 4 KiB, total 16 MiB
-soc_hw_version : Validation success
-machid : Validation success
-Flashing sbl1:                          [ done ]
-Flashing mibib:                         [ done ]
-Flashing bootconfig:                    [ done ]
-Flashing bootconfig1:                   [ done ]
-Flashing tz:                            [  ]
-Flashing devcfg:                        [ done ]
-Flashing ddr-AP-MP03.5-C1_512M16_DDR3:  [ do ]
-Flashing appsblenv:                     [ done ]
-Flashing u-boot:                        [ dxone ]
-Flashing priv_data1:                    [ dox ]
-Flashing ubi:                           [ done ]
-Flashing wifi_fw_ipq5018_qcn6122cs:     [ done ]
-IPQ5018# 
-}
+return 1
+
+
+
