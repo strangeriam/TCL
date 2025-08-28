d273e0a86418c0880065eb494fbd5a54beac1e60
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-08-28 10:39:54 +0800

    Create 11 . Linux fdisk 取出 mmcblk number.tcl

---
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習/11 . Linux fdisk 取出 mmcblk number.tcl
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+set get_info {
+/ # fdisk -l
+Disk /dev/mmcblk[  137.790763] Buffer I/O error on dev mmcblk0rpmb, logical block 0, async page read
+0: 30 GB, 31826378752 bytes, 62160896 sectors
+971264 cylinders, 4 heads, 16 sectors/track
+Units: sectors of 1 * 512 = 512 bytes
+
+Device       Boot StartCHS    EndCHS        StartLBA     EndLBA    Sectors  Size Id Type
+/dev/mmcblk0p1    32,0,1      1023,3,16         2048     346111     344064  168M 83 Linux
+/dev/mmcblk0p3    1023,3,16   1023,3,16       346112   62160895   61814784 29.4G 83 Linux
+Disk /dev/mmcblk0boot1: 31 MB, 33030144 bytes, 64512 sectors
+1008 cylinders, 4 heads, 16 sectors/track
+Units: sectors of 1 * 512 = 512 bytes
+
+Disk /dev/mmcblk0boot1 doesn't contain a valid partition table
+Disk /dev/mmcblk0boot0: 31 MB, 33030144 bytes, 64512 sectors
+1008 cylinders, 4 heads, 16 sectors/track
+Units: sectors of 1 * 512 = 512 bytes
+
+Disk /dev/mmcblk0boot0 doesn't contain a valid partition table
+/ #
+}
