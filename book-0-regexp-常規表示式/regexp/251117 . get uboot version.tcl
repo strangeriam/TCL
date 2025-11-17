617a151de3a354ba39e2d02bdf3bf9691e95575c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-11-17 10:36:30 +0800

    Create 251117 . get uboot version.tcl

---
+++ book-0-regexp-常規表示式/regexp/251117 . get uboot version.tcl
@@ -0,0 +1,14 @@
+set get_info {
+IPQ5018# 
+IPQ5018# version
+
+U-Boot 2016.01-v1.0.1-8389208 (Apr 27 2022 - 02:14:56 +0000)
+arm-openwrt-linux-muslgnueabi-gcc (OpenWrt GCC 5.2.0 3245bbf+r49254) 5.2.0
+GNU ld (GNU Binutils) 2.24.0
+IPQ5018# 
+}
+
+set ::Uboot_ver {Apr 27 2022 - 02:14:56 \+0000}
+regexp $::Uboot_ver $get_info
+
+回應: 1
