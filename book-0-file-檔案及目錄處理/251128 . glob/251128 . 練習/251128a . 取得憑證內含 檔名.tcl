99359b601ad0e136f9bcfbc0797b16b1f762ea4a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-11-28 15:31:40 +0800

    Update 251128a . 取得憑證內含 檔名.tcl

--- book-0-file-檔案及目錄處理/251128 . glob/251128 . 練習/251128a . 取得憑證內含 檔名.tcl
+++ book-0-file-檔案及目錄處理/251128 . glob/251128 . 練習/251128a . 取得憑證內含 檔名.tcl
@@ -1,2 +1,18 @@
-set get_info {
-}
+DOS directory content.
+
+D:\Dropbox\12-Office-SyncMTS-Switch\01_ECS4150_01_SHA\04_ECS4150-28_54PT_V4.1.11r.254.1H.01E_SHA\extapp\FTP_TFTP_AUTO\apcode_certs\902D77217C60>dir
+ 磁碟區 D 中的磁碟是 Lab-D
+ 磁碟區序號:  7097-5025
+
+ D:\Dropbox\12-Office-SyncMTS-Switch\01_ECS4150_01_SHA\04_ECS4150-28_54PT_V4.1.11r.254.1H.01E_SHA\extapp\FTP_TFTP_AUTO\apcode_certs\902D77217C60 的目錄
+
+2025/11/28  下午 03:19    <DIR>          .
+2025/11/28  上午 10:21    <DIR>          ..
+2025/11/13  下午 03:35             1,675 key.pem
+2025/11/13  下午 03:35            24,648 operational.ca
+2025/11/13  下午 03:35             1,119 operational.pem
+               3 個檔案          27,442 位元組
+               2 個目錄  410,579,263,488 位元組可用
+
+D:\Dropbox\12-Office-SyncMTS-Switch\01_ECS4150_01_SHA\04_ECS4150-28_54PT_V4.1.11r.254.1H.01E_SHA\extapp\FTP_TFTP_AUTO\apcode_certs\902D77217C60>
+=====================================
