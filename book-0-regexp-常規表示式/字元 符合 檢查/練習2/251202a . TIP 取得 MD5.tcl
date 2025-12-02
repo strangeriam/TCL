b8323e66a480d332be1d7f8ff7aa4e0a00c9934a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-12-02 16:14:52 +0800

    Update 251202a . TIP 取得 MD5.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/練習2/251202a . TIP 取得 MD5.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習2/251202a . TIP 取得 MD5.tcl
@@ -7,3 +7,22 @@ OpenLAN Demo Birth Certificate MD5 Checksum
   key.pem   : fe48851586655b29cbdcf0d8e013cb56
 Console#
 }
+
+set listitem [list 	cas.pem \
+					cert.pem \
+					cert.id \
+					key.pem ]
+set listmd5 [list 	5bb74a8defd16ec0b679e736b36dde0b \
+					6f1c506bd2db28df1a2b8a01b00a0ae0 \
+					e30b9e500ffedda8f4b4783828f57e9b \
+					fe48851586655b29cbdcf0d8e013cb56]
+
+foreach md5 $listmd5 item $listitem {
+	if { ![regexp -line "${item}\\s+: $md5" $get_info] } {
+		puts "MD5 Check: $item . $md5 ,FAIL"
+		return 0
+	} else {
+		puts "MD5 Check: $item . $md5 ,PASS"
+	}
+}
+
