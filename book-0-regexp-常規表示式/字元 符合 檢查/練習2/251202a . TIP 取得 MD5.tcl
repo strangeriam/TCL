d066f1551ef831f8e7cbf7a2b3653bb5bd58d360
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-12-02 16:11:40 +0800

    Create 251202a . TIP 取得 MD5.tcl

---
+++ book-0-regexp-常規表示式/字元 符合 檢查/練習2/251202a . TIP 取得 MD5.tcl
@@ -0,0 +1,9 @@
+set get_info {
+Console#show ucentral certificate status
+OpenLAN Demo Birth Certificate MD5 Checksum
+  cas.pem   : 5bb74a8defd16ec0b679e736b36dde0b
+  cert.pem  : 6f1c506bd2db28df1a2b8a01b00a0ae0
+  cert-id   : e30b9e500ffedda8f4b4783828f57e9b
+  key.pem   : fe48851586655b29cbdcf0d8e013cb56
+Console#
+}
