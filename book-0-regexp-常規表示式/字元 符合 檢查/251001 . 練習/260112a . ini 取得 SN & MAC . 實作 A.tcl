79adf789f04c710f54d29dc2e0c2195ebf576d10
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-12 17:16:48 +0800

    Update 260112a . ini 取得 SN & MAC . 實作 A.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260112a . ini 取得 SN & MAC . 實作 A.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260112a . ini 取得 SN & MAC . 實作 A.tcl
@@ -7,10 +7,22 @@
 SN --> FSLS715X202511N00123
 MAC --> 649D99940123
 
-;# CODE
-;# ====
-
+;# CODE get SN
+;# ============
 set scanner "2D=2D;SN1;FSLS715X202511N00123;MAC1;649D99940123"
 
+set patternSN {([0-9A-Z]{14})N([0-9]{5})}
+regexp -line $patternSN $scanner SN
+set SN
 
+輸出: FSLS715X202511N00123
 
+;# CODE get MAC
+;# ============
+set scanner "2D=2D;SN1;FSLS715X202511N00123;MAC1;649D99940123"
+
+set patternMAC {(649D99|649D99)([0-9A-F]{6})}
+regexp -line $patternMAC $scanner MAC
+set MAC
+
+輸出: 649D99940123
