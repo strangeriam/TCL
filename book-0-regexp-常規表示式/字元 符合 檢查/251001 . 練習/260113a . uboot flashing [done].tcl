3a614e89b2bed52b272e3a1e2d60f89907ac4641
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-13 10:19:55 +0800

    Create 260113a . uboot flashing [done].tcl

---
+++ book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260113a . uboot flashing [done].tcl
@@ -0,0 +1,37 @@
+set get_info {
+09:21:32:938| ## Executing script at 44000000
+09:21:32:938| 
+09:21:32:938| crc32+ SPI_ADDR_LEN=3
+09:21:32:938| 
+09:21:32:938| SF: Detected W25Q128FW with page size 256 Bytes, erase size 4 KiB, total 16 MiB
+09:21:32:938| 
+09:21:32:938| soc_hw_version : Validation success
+09:21:32:938| 
+09:21:32:938| machid : Validation success
+09:21:32:965| 
+09:21:32:965| Flashing sbl1:                          [ done ]
+09:21:34:013| 
+09:21:34:028| Flashing mibib:                         [ done ]
+09:21:34:280| 
+09:21:34:280| Flashing bootconfig:                    [ done ]
+09:21:34:578| 
+09:21:34:578| Flashing bootconfig1:                   [ done ]
+09:21:34:830| 
+09:21:34:830| Flashing tz:                            [ done ]
+09:21:38:763| 
+09:21:38:763| Flashing devcfg:                        [ done ]
+09:21:39:048| 
+09:21:39:048| Flashing ddr-AP-MP03.5-C1_512M16_DDR3:  [ done ]
+09:21:39:282| 
+09:21:39:282| Flashing appsblenv:                     [ done ]
+09:21:39:707| 
+09:21:39:707| Flashing u-boot:                        [ done ]
+09:21:43:177| 
+09:21:43:177| Flashing priv_data1:                    [ done ]
+09:21:43:480| 
+09:21:43:480| Flashing ubi:                           [ done ]
+09:21:51:699| 
+09:21:51:699| Flashing wifi_fw_ipq5018_qcn6122cs:     [ done ]
+09:21:53:167| 
+09:21:53:167| IPQ5018#
+}
