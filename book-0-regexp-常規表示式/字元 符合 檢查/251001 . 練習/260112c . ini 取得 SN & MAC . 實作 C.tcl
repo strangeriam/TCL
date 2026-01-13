7072bad617cfacf85bde39d4c033f8df17acd6e9
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-13 09:06:30 +0800

    Update 260112c . ini 取得 SN & MAC . 實作 C.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260112c . ini 取得 SN & MAC . 實作 C.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260112c . ini 取得 SN & MAC . 實作 C.tcl
@@ -6,3 +6,25 @@
 需輸出
 SN --> EC2231009930
 MAC --> E001A63FB622
+
+
+;# CODE get SN
+;# ============
+set scanner "2D=2D;SN1;EC2430001796;MAC1;A01AE3F90050"
+
+set patternSN {EC([0-9A-Z]{10})}
+regexp -line $patternSN $scanner SN
+set SN
+
+輸出: EC2430001796
+
+
+;# CODE get MAC
+;# ============
+set scanner "2D=2D;SN1;EC2430001796;MAC1;A01AE3F90050"
+
+set patternMAC {(A01AE3|649D99)([0-9A-F]{6})}
+regexp -line $patternMAC $scanner MAC
+set MAC
+
+輸出: A01AE3F90050
