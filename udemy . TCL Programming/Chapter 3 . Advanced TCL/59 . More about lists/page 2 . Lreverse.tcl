ee25983d9ceef7ea5ecf0f440289dedf230f4275
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-21 17:03:37 +0800

    Create page 2 . Lreverse.tcl

---
+++ udemy . TCL Programming/Chapter 3 . Advanced TCL/59 . More about lists/page 2 . Lreverse.tcl
@@ -0,0 +1,9 @@
+The lreverse command is used to reverse the order of elements in a list.
+Syntax: lreverse list
+The command returns a new list with the elements in reverse order.
+
+Example:
+set numbers {1 2 3 4 5}
+set reversed [lreverse $numbers]
+puts $reversed
+# Output: 5 4 3 2 1
