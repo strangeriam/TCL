b9a9555c0a6259ac422b4e867eb017c79bcfb393
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-22 10:49:46 +0800

    Create 59d . More about lists . Lmap.tcl

---
+++ udemy . TCL Programming/Chapter 3B . Advanced TCL . LIST/59d . More about lists . Lmap.tcl
@@ -0,0 +1,11 @@
+This is a command that allows programmer to apply a given script or lambda expression to all the elements in a list,
+return a list containing the result.
+
+Syntax: lmap varName list script
+
+Example:
+set numbers {1 2 3 4 5}
+set doubled [lmap n $numbers {expr {$n * 2}}]
+puts $doubled
+
+;# Output: 2 4 6 8 10
