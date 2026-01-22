9fa75a36cc362c22cc1deac7a7e2644013a0ba45
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-22 09:24:37 +0800

    Create 59c . More about lists . Lsearch command.tcl

---
+++ udemy . TCL Programming/Chapter 3B . Advanced TCL . LIST/59c . More about lists . Lsearch command.tcl
@@ -0,0 +1,17 @@
+
+
+=======================================================
+The lsearch command allows the programmer to efficiently get all indexes what contain a value,
+or -1 if no matches are found.
+
+Starting with TCL version 8.4,
+the lsearch command has been added new features to optimize searches in sorted lists,
+with -sort option.
+It also has a -int option to not convert the element to strings, which is a faster approach.
+
+Basic lsearch syntax is as follows:
+learch ?option? <list> <expr>
+
+Example:
+lsearch {hello my friend} f* => 2
+lsearch -inline {hello my friend} f* => friend
