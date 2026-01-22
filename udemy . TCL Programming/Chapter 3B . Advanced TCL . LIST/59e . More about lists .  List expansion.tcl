b892fa5edf0d44734b2511ff4490d83bb522ce09
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-22 10:59:24 +0800

    Update 59e . More about lists .  List expansion.tcl

--- udemy . TCL Programming/Chapter 3B . Advanced TCL . LIST/59e . More about lists .  List expansion.tcl
+++ udemy . TCL Programming/Chapter 3B . Advanced TCL . LIST/59e . More about lists .  List expansion.tcl
@@ -1,10 +1,14 @@
+context 情境 前後關系
+arguments 論點
+rather than 而不是
+entity 實體
 
 
 
 =================================================
 In TCL,
 list expansion refers to the process of expanding a list into individual elements,
-usuallu in the context of command arguments or variable assignments.
+usually in the context of command arguments or variable assignments.
 It allows you to treat a list as separate elements rather than a single entity.
 
 List expansion can be done using the {*} syntax, also know as the "list expansion operator".
