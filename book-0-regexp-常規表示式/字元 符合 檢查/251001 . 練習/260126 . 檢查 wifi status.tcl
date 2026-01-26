c1dabfd4f5645d09c93779fb1fbf05d1aa0f0e6b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-26 13:41:50 +0800

    Update 260126 . 檢查 wifi status.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260126 . 檢查 wifi status.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260126 . 檢查 wifi status.tcl
@@ -1,1 +1,25 @@
 aaa
+
+
+
+
+
+
+set get_info {
+AP-N506H-0123:~# ifconfig wlan0 && ifconfig wlan1
+wlan0     Link encap:Ethernet  HWaddr 64:9D:99:94:01:25  
+          UP BROADCAST MULTICAST  MTU:1500  Metric:1
+          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:4096 
+          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
+wlan1     Link encap:Ethernet  HWaddr 64:9D:99:94:01:26  
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:3 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:4096 
+          RX bytes:0 (0.0 B)  TX bytes:848 (848.0 B)
+
+AP-N506H-0123:~# 
+}
