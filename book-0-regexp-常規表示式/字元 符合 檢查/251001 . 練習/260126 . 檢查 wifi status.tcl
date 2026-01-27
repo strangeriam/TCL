8d7360d356e5784d81698de16391ee3bf2980623
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-27 09:43:10 +0800

    Update 260126 . 檢查 wifi status.tcl

--- book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260126 . 檢查 wifi status.tcl
+++ book-0-regexp-常規表示式/字元 符合 檢查/251001 . 練習/260126 . 檢查 wifi status.tcl
@@ -7,7 +7,14 @@ wlan1: 是否為 "UP BROADCAST RUNNING MULTICAST"
 
 ====================================================
 set get_info {
-AP-N506H-0123:~# ifconfig wlan0 && ifconfig wlan1
+AP-N506H-0123:~# ifconfig
+up0v0     Link encap:Ethernet  HWaddr 64:9D:99:94:01:23  
+          UP BROADCAST MULTICAST  MTU:1500  Metric:1
+          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
 wlan0     Link encap:Ethernet  HWaddr 64:9D:99:94:01:25  
           UP BROADCAST MULTICAST  MTU:1500  Metric:1
           RX packets:0 errors:0 dropped:0 overruns:0 frame:0
@@ -18,9 +25,9 @@ wlan0     Link encap:Ethernet  HWaddr 64:9D:99:94:01:25
 wlan1     Link encap:Ethernet  HWaddr 64:9D:99:94:01:26  
           UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
           RX packets:0 errors:0 dropped:0 overruns:0 frame:0
-          TX packets:3 errors:0 dropped:0 overruns:0 carrier:0
+          TX packets:5 errors:0 dropped:0 overruns:0 carrier:0
           collisions:0 txqueuelen:4096 
-          RX bytes:0 (0.0 B)  TX bytes:848 (848.0 B)
+          RX bytes:0 (0.0 B)  TX bytes:808 (808.0 B)
 
-AP-N506H-0123:~# 
+AP-N506H-0123:~#
 }
