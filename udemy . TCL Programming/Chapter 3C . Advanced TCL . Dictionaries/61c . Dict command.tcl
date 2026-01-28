a4cff4acee558fde7816d5a6c731ff55db5944e8
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-28 14:41:52 +0800

    Create 61c . Dict command.tcl

---
+++ udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/61c . Dict command.tcl
@@ -0,0 +1,12 @@
+
+
+
+=============================
+dict update dictName ?key? varname script - 
+- Update values within a dictionary based on a key.
+- It allows you to modify values of existing keys without creating a new dictionary.
+
+
+dict with dictName script - 
+- Access the values of a dictionary within a script block without explicitly referencing the dictionary variable.
+- This command does not allow program to make changes in the dictionary values.
