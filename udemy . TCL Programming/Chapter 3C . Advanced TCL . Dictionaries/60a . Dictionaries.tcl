8aab1dbc36d47cc5b88c0ac2f809460e52030b72
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-28 11:11:16 +0800

    Update 60a . Dictionaries.tcl

--- udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/60a . Dictionaries.tcl
+++ udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/60a . Dictionaries.tcl
@@ -1,1 +1,9 @@
-aaa
+
+
+
+
+
+================================================
+TCL arrays are collection of variables, so we cannot pass them directly to procedures as a value.
+We need to convert them to values using array get, or else use the upvar command to create an alias for the array.
+Arrays connot be included in other data structures like lists.
