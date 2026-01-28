b5119543e19f04d9994474fd8cd1be47f04ac6db
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-28 13:54:03 +0800

    Update 60a . Dictionaries.tcl

--- udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/60a . Dictionaries.tcl
+++ udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/60a . Dictionaries.tcl
@@ -7,3 +7,13 @@
 TCL arrays are collection of variables, so we cannot pass them directly to procedures as a value.
 We need to convert them to values using array get, or else use the upvar command to create an alias for the array.
 Arrays connot be included in other data structures like lists.
+
+TCL dictionaries are very similar to arrays, but they are key-value pairs of pure values.
+This allows dictionaries to be passed directly to procedures.
+Unlike arrays, dictionaries can be nested, so that the value of a particular key can be another dictionary.
+Programmer can use dictionaries to build complex data structures.
+
+Two ways to create dictionaries:
+dict set dictname key value
+or
+dict create dictname key1 value1 key2 value2 ...
