861c47ec273611978c4f83103042ffcedd13b99d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-28 14:22:00 +0800

    Update 61a . Dict commands.tcl

--- udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/61a . Dict commands.tcl
+++ udemy . TCL Programming/Chapter 3C . Advanced TCL . Dictionaries/61a . Dict commands.tcl
@@ -17,3 +17,21 @@ dict for {key value} $dictname - Iterates over all the key-value pairs from dict
 dict filter $dictanme script {k v} { FILTER EXPR }
 dict filter $dictname key <KEYS>
  - Creates a new dictionary that contains a subset of key-value pairs based on the filter condition
+
+
+dict incr dictName key ?incr? - Increment the key (default incr is 1)
+dict lappend dictName key value - 
+- Appends the list of values to the list in the given key.
+- Returns the updated dictionary
+
+dict map {keyVar} valueVar} dictName body - 
+- Similar to lmap, this command transforms all key-value pairs in a dict.
+- Returns a new dict.
+
+dict merge dictNames - 
+- Returns a dictionary that contains the merged contents of each dictName provided.
+- If the same key is used by more than one dict, the latter is preserved.
+
+dict remove dictName keys - Removes key from the dictName and returns a new dict.
+dict replace dictName key value - Returns a new dictionary with updated key-value
+dict unset dictName keys - Returns an updated dictionary with the given keys removed
