9a021620c6912f9ba6d16ca6ab3301f688870689
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-30 08:31:51 +0800

    Create 64b . Try-on-finally blocks.tcl

---
+++ udemy . TCL Programming/Chapter 3D . Debug/64b . Try-on-finally blocks.tcl
@@ -0,0 +1,22 @@
+
+
+==================================================
+The try-catch block provides a more structured way to handle errors by grounping a block of code
+that may potentially raise errors and provides a mechanism to handle those errors in a controller manner.
+
+The syntax of such blocks is as follows:
+
+try {
+    // Code that may raise errors
+} on error { varName } {
+    // Error handling code
+} finally {
+    // Code to execute regardless of whether an error occurfed or not
+}
+
+set f [open /some/file/name a]
+try {
+    puts $f "some message"
+} {
+    close $f
+}
