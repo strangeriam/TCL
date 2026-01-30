48a0436ddd4a13eede6bff6d090269743255aae2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-30 14:39:39 +0800

    Create 68f . Practice for eval command.tcl

---
+++ udemy . TCL Programming/Chapter 3F . Command . eval/68f . Practice for eval command.tcl
@@ -0,0 +1,18 @@
+
+
+===============================================
+Write a TCL script to calculate the sum of a list of numbers.
+The list of numbers will be provided as a string input, where each number is separated by a space.
+The script should perform the following steps:
+
+Prompt the user to enter a list of numbers, separated by spaces.
+
+Attenpt to calculate the sum of the numbers without using the eval command.
+
+Observe the errors or unexpected behavior that occur when trying to perform the sum without eval.
+
+Rewrite the code using the eval command, but without using list-based evaluation.
+
+Observe the improved behavior when using eval.
+
+Finally, rewrite the code again using list-basesd evaluation to construct and evaluate the command.
