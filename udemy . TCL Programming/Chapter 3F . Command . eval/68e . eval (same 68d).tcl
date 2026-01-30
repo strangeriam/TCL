93f1626bd81374ae8134e434a685bceb42d92aff
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-30 14:32:25 +0800

    Create 68e . eval (same 68d).tcl

---
+++ udemy . TCL Programming/Chapter 3F . Command . eval/68e . eval (same 68d).tcl
@@ -0,0 +1,17 @@
+
+=====================================================
+The eval command can be tricky to use corrently, especially when dialing with complex or dynamically generated code.
+One common pitfall is the accidental evaluation of characters or strings as TCL code,
+which can lead to unexpected behavior or security valnerabilities.
+
+To mitigate these risks, you can use lists to construct and execute commands.
+This technique is commonly referred to as "list-based command construction" or "list-based evaluation" 
+and is generally considered safer and more robust than using the eval command directly with string manipulation.
+
+In TCL, a list is a sequence of TCL words enclosed in curly braces or separacted by whitespace.
+By using lists, you can construct commands without the need for string manioulation, 
+which helps prevent unintended evaluation of special characters or unexpected code injection.
+
+The list command takes any number of arguments and returns a properly formatted list.
+Each argument is treated as a separate as a separate word in the resulting list,
+ensuring that special characters or spaces are properly escaped or quoted.
