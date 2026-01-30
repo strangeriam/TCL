e9cb7ea240ae60f529c0700c2084aa766c61dbf3
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-30 14:30:13 +0800

    Create 68d . eval.tcl

---
+++ udemy . TCL Programming/Chapter 3F . Command . eval/68d . eval.tcl
@@ -0,0 +1,19 @@
+
+
+
+=================================================
+Example of eval command:
+
+set x 10
+set y 20
+set script {set z [expr $x + $y]}
+eval $script
+puts "The value of z is: $z"
+
+In this example, the eval command evaluates the script contained in the script variable,
+which sets the value of z to the sum of x and y.
+The value of z is then printed, resulting in the output: "The value of z is: 30".
+
+It's important to be cautious when using the eval command, 
+as it can execute arbitrary TCL code and potentially introduce security risks or code vulnerabilities if used improperly.
+It's recommended to sanitize and validate the input string passed to eval to ensure it contains only trusted and expected code.
