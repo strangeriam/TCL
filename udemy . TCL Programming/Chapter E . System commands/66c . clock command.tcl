fb61feb47638d34c1cf4a4d21eac90effdba2ae8
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-01-30 09:26:19 +0800

    Update and rename 66c . clock command.txt to 66c . clock command.tcl

---
+++ udemy . TCL Programming/Chapter E . System commands/66c . clock command.tcl
@@ -0,0 +1,31 @@
+
+
+
+==========================================
+There is a clock command that provides functions for obtaining and manipulating the system time,
+including date-related operations.
+The clock command allows you to work with dates, times, and timestamps.
+
+Command - Description - Example
+================================
+
+clock format . 
+Formats a timestamp into a human-readable date string. - 
+Example: set currentDate [clock format [clock seconds] -format "%Y-%m-%d"]
+
+clock scan . 
+Parses a date string and converts it into a timestamp. - 
+Example: set timestamp [clock scan "2022-01-01" -format "%Y-%m-%d"]
+
+clock seconds . 
+Returns the current system time as a timestamp. - 
+Example: set currentTime [clock seconds]
+
+clock add . 
+Adds or subtracts a specified amount of time from a timestamp. - 
+Example: set futureTime [clock add $currentTime 1 day]
+
+clock formatTZ . 
+Formats a timestamp with a specitie timezone. - 
+Example: set formattedTime [clock formatTZ $currentTime - timezone "America/New_York"]
+
