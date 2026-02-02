2c8069e3a906d13b63506ede873fed0610605bb6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-02-02 10:06:28 +0800

    Create 260202 . uboot version 判斷錯誤.tcl

---
+++ book-0-regexp-常規表示式/regexp/260202 . uboot version 判斷錯誤.tcl
@@ -0,0 +1,9 @@
+
+set get_info {
+version
+08:51:12:437| U-Boot 2023.01-1.0.4 (Jun 17 2024 - 18:21:03 +0800)
+08:51:12:437| 
+08:51:12:437| aarch64-linux-gnu-gcc (GNU Toolchain for the A-profile Architecture 8.3-2019.03 (arm-rel-8.36)) 8.3.0
+08:51:12:437| GNU ld (GNU Toolchain for the A-profile Architecture 8.3-2019.03 (arm-rel-8.36)) 2.32.0.20190321
+08:51:12:437| Marvell>> 
+}
