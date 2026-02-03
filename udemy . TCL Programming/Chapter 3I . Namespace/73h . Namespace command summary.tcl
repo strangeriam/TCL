8c61bdf5f11236861faf96d6f112892505a9e044
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-02-03 14:21:27 +0800

    Update 73h . Namespace command summary.tcl

--- udemy . TCL Programming/Chapter 3I . Namespace/73h . Namespace command summary.tcl
+++ udemy . TCL Programming/Chapter 3I . Namespace/73h . Namespace command summary.tcl
@@ -5,3 +5,10 @@
 ===============================================
 Command -- Description
 
+namespace exists name --> Checks if namespace name exists
+namespace delete --> Delete a namespace and all its content
+namespace which --> Returns the fully qualified name of a command
+namespace qualifiersof --> Retuens the namespace qualifiers of a command
+namespace origin --> Returns the fully qualified name of the orifinating command
+namespace forget --> Equivalent to undo a namespace import
+namespace code name script --> Executes script within a specific namespace context
