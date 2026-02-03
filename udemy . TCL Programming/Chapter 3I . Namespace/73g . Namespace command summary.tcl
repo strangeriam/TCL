81c853d368484f446904dfbf52042d1b21551261
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-02-03 14:15:57 +0800

    Create 73g . Namespace command summary.tcl

---
+++ udemy . TCL Programming/Chapter 3I . Namespace/73g . Namespace command summary.tcl
@@ -0,0 +1,14 @@
+
+
+
+=========================================================
+Command -- Description
+
+namespace eval --> Create and evaluates scripts within a namespace
+namespace current --> Returns the fully qualified name of the current namespace
+namespace parent --> Returns the parent namespace of the current namespace
+namespace children --> Returns a list of child namespaces of a given namespace
+namespace qualifiers --> Returns the namespace qualifiers of a fiven namespace
+namespace path --> Returns or set the namespace search path
+namespace import --> Imports commands from another namespace
+namespace export --> Exports commands to be accesible in a namespace import
