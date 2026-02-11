03906ea9ac3b039255a5ba8e440e3fa0d3f7aa3c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-02-11 10:28:54 +0800

    Update 68c . eval.tcl

--- udemy . TCL Programming/Chapter 3F . eval/68c . eval.tcl
+++ udemy . TCL Programming/Chapter 3F . eval/68c . eval.tcl
@@ -1,9 +1,20 @@
 
+tricky 棘手
+complex 複雜的
+pitfall 陷阱
+accidental 偶發
+valnerabilities 漏洞
+
 =====================================================
 The eval command can be tricky to use corrently, especially when dialing with complex or dynamically generated code.
 One common pitfall is the accidental evaluation of characters or strings as TCL code,
 which can lead to unexpected behavior or security valnerabilities.
 
+
+mitigate 緩解
+construct 構造
+
+======================================================
 To mitigate these risks, you can use lists to construct and execute commands.
 This technique is commonly referred to as "list-based command construction" or "list-based evaluation" 
 and is generally considered safer and more robust than using the eval command directly with string manipulation.
