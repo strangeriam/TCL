5231742be0b5f871a47c60c69d47d75a3f39b0c0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-10 13:41:21 +0800

    Update 260310 . 60G CSV Insert.tcl

--- Training/260310 . 60G CSV Insert.tcl
+++ Training/260310 . 60G CSV Insert.tcl
@@ -1,9 +1,9 @@
 
 set ::path_report D:\/worktmp\/REPORT
 set ::resultFile $::path_report\/report.csv
-
 if { ![file exist $::path_report] } { file mkdir $::path_report }
 
+;# ================================================
 set build_date_time [clock format [clock seconds] -format "%y/%m/%d_%H:%M:%S"]
 set build_date [lindex [string map {_ " "} $build_date_time] 0]
 set build_time [lindex [string map {_ " "} $build_date_time] 1]
