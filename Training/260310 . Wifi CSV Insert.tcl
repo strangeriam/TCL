88fab689e87eb7cabc87639cfb006a122c699a27
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-10 11:33:18 +0800

    Update 260310 . Wifi CSV Insert.tcl

--- Training/260310 . Wifi CSV Insert.tcl
+++ Training/260310 . Wifi CSV Insert.tcl
@@ -4,4 +4,9 @@ set ::resultFile $::path_report\/report.csv
 
 if { ![file exist $::path_report] } { file mkdir $::path_report }
 
+set build_date_time [clock format [clock seconds] -format "%y/%m/%d_%H:%M:%S"]
+set build_date [lindex [string map {_ " "} $build_date_time] 0]
+set build_time [lindex [string map {_ " "} $build_date_time] 1]
+
+
 if {[info exists ::csvData]} { unset ::csvData }
