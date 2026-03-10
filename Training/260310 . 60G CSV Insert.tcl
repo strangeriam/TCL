9e0a71d8fb370ea7ca6bd6c64d2b7e897da273f8
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-10 13:40:50 +0800

    Add build date and time variables to TCL script

--- Training/260310 . 60G CSV Insert.tcl
+++ Training/260310 . 60G CSV Insert.tcl
@@ -8,6 +8,8 @@ set build_date_time [clock format [clock seconds] -format "%y/%m/%d_%H:%M:%S"]
 set build_date [lindex [string map {_ " "} $build_date_time] 0]
 set build_time [lindex [string map {_ " "} $build_date_time] 1]
 
+;# ================================================
+
 set itemlist [list SN DATE TIME BRD_VER CHANNEL SECTOR PHASE ANTENNA "" TEMP_RF PW_TX PW_BG_START PW_BG_END]
 
 foreach item $itemlist {
