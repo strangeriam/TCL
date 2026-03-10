eb073fdefb71cdc94fd1ac095085c7227adf9870
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-10 11:36:15 +0800

    Update 260310 . Wifi CSV Insert.tcl

--- Training/260310 . Wifi CSV Insert.tcl
+++ Training/260310 . Wifi CSV Insert.tcl
@@ -33,5 +33,18 @@ if { ! [file exists $::resultFile] } {
 
 
 
+proc _f_WriteFile { fname access data } {
+	if { 	[catch {
+			set fd [open $fname $access]
+			puts -nonewline $fd $data
+			close $fd
+		} retmsg] } {
+
+  		return 0
+	} else {
+		return 1
+	}
+}
+
 
 if {[info exists ::csvData]} { unset ::csvData }
