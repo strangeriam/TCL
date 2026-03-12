53df5f995804baa4a33a15b6e497b382bb79ce94
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-12 11:04:19 +0800

    Update 260312 . license 2026 . check profile content.tcl

--- book-0-regexp-常規表示式/260202 練習/260312 . license 2026 . check profile content.tcl
+++ book-0-regexp-常規表示式/260202 練習/260312 . license 2026 . check profile content.tcl
@@ -1,1 +1,8 @@
-260312 . license 2026 . check profile content.tcl
+set get_info {
+Console#show license file
+ID Expired Date Feature
+-- ------------ ------------------------------------------------
+ 1 2026-05-24   Essential, cloud-m
+
+Input ID to show detail: 
+}
