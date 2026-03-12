14f64f4a74a1f71a45c17b16de93435c2a4d267d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-12 11:05:28 +0800

    Update 260312 . license 2026 . check profile content.tcl

--- book-0-regexp-常規表示式/260202 練習/260312 . license 2026 . check profile content.tcl
+++ book-0-regexp-常規表示式/260202 練習/260312 . license 2026 . check profile content.tcl
@@ -1,3 +1,7 @@
+set ::lic_profile 				"Essential, cloud-m"
+set ::lic_profiledigest 		"da407b2c4c92090ced9e3b9c5f8747ab"
+set ::lic_projectname 			ECS5550
+
 set get_info {
 Console#show license file
 ID Expired Date Feature
