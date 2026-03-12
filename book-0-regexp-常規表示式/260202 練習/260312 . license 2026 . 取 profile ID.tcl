ce1743b40ded1c49be04710c8aeca656318a7721
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-12 17:28:54 +0800

    Update 260312 . license 2026 . 取 profile ID.tcl

--- book-0-regexp-常規表示式/260202 練習/260312 . license 2026 . 取 profile ID.tcl
+++ book-0-regexp-常規表示式/260202 練習/260312 . license 2026 . 取 profile ID.tcl
@@ -18,14 +18,38 @@ Input ID to show detail:
 set pattern {[0-9]+\s+[0-9]{4}-[0-9]{2}-[0-9]{2}\s+[a-zA-Z\+,]+\s[a-z]+-\D}
 ;# or
 set pattern {[0-9]+\s+[0-9]{4}-[0-9]{2}-[0-9]{2}\s+[a-zA-Z\+,]+\s[a-z-]+}
+
 set aaa [regexp -all -inline -- $pattern $get_info]
 ;# 輸出: {1 2026-05-24   Essential, cloud-m}
 
 set bbb [lindex $aaa 0]
 ;# 輸出: 1 2026-05-24   Essential, cloud-m
 
-set id [lindex $bbb 0]
-;# 輸出: 1
+if {[regexp -linestop .*$::lic_profile $bbb]} {
+   regexp -linestop .*$::lic_profile $bbb tmp
+   set id [lindex $tmp 0]
+   ;# 輸出: 1
+} else {
+   return 0
+}
+
+;# OR
+
+set aaa [regexp -all -inline -- $pattern $get_info]
+if {[regexp $::lic_profile $aaa]} {
+    if {[regexp -linestop .*$::lic_profile $aaa]} {
+       set id_tmp [lindex $aaa 0] ;# 輸出: 1 2026-05-24   Essential, cloud-m
+       regexp -linestop .*$::lic_profile $id_tmp tmp
+       set id [lindex $tmp 0]  ;# 輸出: 1
+    }
+} else {
+   return 0
+}
+    
+
+
+
+
 
 ;# REGEXP 解說:
 ;# ===================================
