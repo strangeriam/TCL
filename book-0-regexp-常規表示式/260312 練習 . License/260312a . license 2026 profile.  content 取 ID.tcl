810ca278f7b8373904eba0e8ae57d8ea4b5df6c2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-13 14:14:57 +0800

    Update 260312a . license 2026 profile.  content 取 ID.tcl

--- book-0-regexp-常規表示式/260312 練習 . License/260312a . license 2026 profile.  content 取 ID.tcl
+++ book-0-regexp-常規表示式/260312 練習 . License/260312a . license 2026 profile.  content 取 ID.tcl
@@ -1,6 +1,4 @@
 set ::lic_profile 				"Essential, cloud-m"
-set ::lic_profiledigest 		"da407b2c4c92090ced9e3b9c5f8747ab"
-set ::lic_projectname 			ECS5550
 
 set get_info {
 Console#show license file
