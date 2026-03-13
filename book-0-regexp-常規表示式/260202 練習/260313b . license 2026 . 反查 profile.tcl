3977806479bd37afe55c060922d105be42c0b872
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-13 10:47:16 +0800

    Update 260313b . license 2026 . 反查 profile.tcl

--- book-0-regexp-常規表示式/260202 練習/260313b . license 2026 . 反查 profile.tcl
+++ book-0-regexp-常規表示式/260202 練習/260313b . license 2026 . 反查 profile.tcl
@@ -1,1 +1,39 @@
-260313 . license 2026 . 檢查 profile.tcl
+目的: 查出字串 "Essential, cloud-m" 前的 Item 是否為 "Current Image Profile:"
+
+set ::lic_profile 				"Essential, cloud-m"
+
+set get_info {
+10:35:52:748| Console#show license
+10:35:52:822| 
+10:35:52:822| Current Image Profile:
+10:35:52:822| 
+10:35:52:822|   Essential, cloud-m
+10:35:52:822| 
+10:35:52:822| Active functions:
+10:35:52:822| 
+10:35:52:822|   
+10:35:52:822| 
+10:35:52:822| Inactive functions:
+10:35:52:822| 
+10:35:52:822|   L3 Premium
+10:35:52:822| 
+10:35:52:822| Console#
+}
+
+;# ===================================
+set pattern {Current Image Profile:\n\s+[a-zA-Z\+,]+\s[a-z-]+}
+
+;# Step 1: 取出
+Current Image Profile:
+  Essential, cloud-m
+
+set aaa [regexp -all -inline -- $pattern $get_info]
+;# 輸出: 
+Current Image Profile:
+  Essential, cloud-m
+
+if {[regexp $::lic_profile $aaa]} {
+    return 1
+} else {
+    return 0
+}
