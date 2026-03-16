198e8bdadbc055f270d377c62bf1e35f34c7a065
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-16 14:17:08 +0800

    Update 260316 . license 2026 Active functions . 反查.tcl

--- book-0-regexp-常規表示式/260312 練習 . License/260316 . license 2026 Active functions . 反查.tcl
+++ book-0-regexp-常規表示式/260312 練習 . License/260316 . license 2026 Active functions . 反查.tcl
@@ -1,1 +1,20 @@
 目的: 查出字串 "L3 Premium" 前的 Item 是否為 "Active functions:"
+
+
+set get_info {
+14:15:53:975| Console#show license
+14:15:54:020| 
+14:15:54:020| Current Image Profile:
+14:15:54:045| 
+14:15:54:045|   Essential, cloud-m
+14:15:54:045| 
+14:15:54:045| Active functions:
+14:15:54:045| 
+14:15:54:045|   L3 Premium
+14:15:54:045| 
+14:15:54:045| Inactive functions:
+14:15:54:045| 
+14:15:54:045|   
+14:15:54:045| 
+14:15:54:045| Console#
+}
