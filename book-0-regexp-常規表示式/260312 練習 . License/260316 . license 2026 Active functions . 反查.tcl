28bf75833beea378815405be4411dc55ec0c2742
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-16 14:26:04 +0800

    Update 260316 . license 2026 Active functions . 反查.tcl

--- book-0-regexp-常規表示式/260312 練習 . License/260316 . license 2026 Active functions . 反查.tcl
+++ book-0-regexp-常規表示式/260312 練習 . License/260316 . license 2026 Active functions . 反查.tcl
@@ -19,3 +19,26 @@ set get_info {
 14:15:54:045| 
 14:15:54:045| Console#
 }
+
+;# ===================================
+;# 刪除每行時間 "10:35:52:822" 和 "|"
+set pattern_eraseTime {[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{3}\|}
+set aaa [regsub -all -line $pattern_eraseTime $get_info ""]
+
+set pattern {Inactive functions:\n\s+[A-Z3]+\s[a-zA-Z]+}
+
+;# Step 1: 取出
+ Inactive functions:
+ 
+   L3 Premium
+
+set bbb [lindex [regexp -all -inline -- $pattern $aaa] 0]
+;# 輸出: 
+Inactive functions:
+   L3 Premium
+
+if {[regexp $::lic_function $bbb]} {
+    return 1
+} else {
+    return 0
+}
