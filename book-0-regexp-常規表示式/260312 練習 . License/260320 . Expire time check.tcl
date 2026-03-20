f3a70e5a30e1792c2cf2c5f33a266af1893943b2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-20 08:38:22 +0800

    Update 260320 . Expire time check.tcl

--- book-0-regexp-常規表示式/260312 練習 . License/260320 . Expire time check.tcl
+++ book-0-regexp-常規表示式/260312 練習 . License/260320 . Expire time check.tcl
@@ -1,3 +1,6 @@
 
-set LIC_test "test_lic/PROFILE_ECS5550\_A01AE32DEDB8.lic"
+set mac A01AE32DEDB8
+set LIC_test "./extapp/FTP_TFTP_AUTO/ec_license/test_lic/PROFILE_$::lic_projectname\_$mac.lic"
+
+if { ![file exists $LIC_test]} { usermsg "WRONG !!!\nNo $LIC_test" }
 set get_info [_f_ReadFile $LIC_test]
