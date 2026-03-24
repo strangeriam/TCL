2ac275eb109ee8cb3b07d22f9a52cc54546383dd
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-24 13:56:35 +0800

    Update 260324 . Switch Simba License eth check.tcl

--- book-0-regexp-常規表示式/260202 練習/260324 . Switch Simba License eth check.tcl
+++ book-0-regexp-常規表示式/260202 練習/260324 . Switch Simba License eth check.tcl
@@ -1,4 +1,51 @@
 
+set ::num_port_check 30
+
+for {set num 1} {$num <= $::num_port_check} {incr num} {
+		if { ! [regexp "Eth 1/[format %2s $num]+\\\s+Up" $get_info] } {
+			if { ! [regexp "Eth 1/[format %2s $num]+\\\s+Down" $get_info] } {
+				puts "Check Eth1/[format %2s $num] lic Status ,FAIL"
+				return 0
+			} else {
+				puts "Check Eth1/[format %2s $num] lic Status ,PASS"
+			}
+		} else {
+			puts "Check Eth1/[format %2s $num] lic Status ,PASS"
+		}
+}
+
+;# 輸出:
+Check Eth1/ 1 lic Status ,PASS
+Check Eth1/ 2 lic Status ,PASS
+Check Eth1/ 3 lic Status ,PASS
+Check Eth1/ 4 lic Status ,PASS
+Check Eth1/ 5 lic Status ,PASS
+Check Eth1/ 6 lic Status ,PASS
+Check Eth1/ 7 lic Status ,PASS
+Check Eth1/ 8 lic Status ,PASS
+Check Eth1/ 9 lic Status ,PASS
+Check Eth1/10 lic Status ,PASS
+Check Eth1/11 lic Status ,PASS
+Check Eth1/12 lic Status ,PASS
+Check Eth1/13 lic Status ,PASS
+Check Eth1/14 lic Status ,PASS
+Check Eth1/15 lic Status ,PASS
+Check Eth1/16 lic Status ,PASS
+Check Eth1/17 lic Status ,PASS
+Check Eth1/18 lic Status ,PASS
+Check Eth1/19 lic Status ,PASS
+Check Eth1/20 lic Status ,PASS
+Check Eth1/21 lic Status ,PASS
+Check Eth1/22 lic Status ,PASS
+Check Eth1/23 lic Status ,PASS
+Check Eth1/24 lic Status ,PASS
+Check Eth1/25 lic Status ,PASS
+Check Eth1/26 lic Status ,PASS
+Check Eth1/27 lic Status ,PASS
+Check Eth1/28 lic Status ,PASS
+Check Eth1/29 lic Status ,PASS
+Check Eth1/30 lic Status ,PASS
+
 
 set get_info {
 Console#terminal length 0
