dba43b20aa402e0efe6f8dc0809f658c2491b56e
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-24 13:45:03 +0800

    Create 260324 . Switch Simba License eth check.tcl

---
+++ book-0-regexp-常規表示式/260202 練習/260324 . Switch Simba License eth check.tcl
@@ -0,0 +1,39 @@
+
+
+set get_info {
+Console#terminal length 0
+Console#show interfaces brief
+Interface Name              Status    PVID Pri Speed/Duplex  Type         Trunk
+--------- ----------------- --------- ---- --- ------------- ------------ -----
+Eth 1/ 1                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 2                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 3                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 4                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 5                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 6                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 7                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 8                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/ 9                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/10                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/11                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/12                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/13                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/14                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/15                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/16                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/17                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/18                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/19                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/20                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/21                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/22                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/23                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/24                    Down         1   0 10Gfull       10GBASE SFP+ None 
+Eth 1/25                    Down         1   0 100Gfull      100GBASE QSFP None 
+Eth 1/26                    Down         1   0 100Gfull      100GBASE QSFP None 
+Eth 1/27                    Down         1   0 100Gfull      100GBASE QSFP None 
+Eth 1/28                    Down         1   0 100Gfull      100GBASE QSFP None 
+Eth 1/29                    Down         1   0 100Gfull      100GBASE QSFP None 
+Eth 1/30                    Down         1   0 100Gfull      100GBASE QSFP None 
+Console#
+}
