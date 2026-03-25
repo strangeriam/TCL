74d912a6e0ec91b88781db77f61e17747a24a4c9
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-25 10:09:55 +0800

    Update 260325a . 正式版 License Profile 取 ID.tcl

--- book-0-regexp-常規表示式/260312 練習 . License/260325a . 正式版 License Profile 取 ID.tcl
+++ book-0-regexp-常規表示式/260312 練習 . License/260325a . 正式版 License Profile 取 ID.tcl
@@ -1,1 +1,10 @@
-sfasfdasd
+
+set get_info {
+show license file
+ID Expired Date Feature
+-- ------------ ------------------------------------------------
+ 1 Permanent    Essential, cloud-m
+ 2 Permanent    L3 Premium
+Input ID to show detail:
+Console#
+}
