3b1b7b611a1c91ce16446e7d4c33df58012a62e5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-03-25 10:32:58 +0800

    Update 260325a . 正式版 License Profile 取 ID.tcl

--- book-0-regexp-常規表示式/260312 練習 . License/260325a . 正式版 License Profile 取 ID.tcl
+++ book-0-regexp-常規表示式/260312 練習 . License/260325a . 正式版 License Profile 取 ID.tcl
@@ -29,7 +29,7 @@ Console#
 ;# ===================================
 ;# Step 1: 取得 profile 所在的 ID.
 set pattern {[0-9]\s[0-9]{4}-[0-9]{2}-[0-9]{2}\s+[a-zA-Z\+,]+\s[a-z-]+}
-set pattern {[0-9]\s     \s+[a-zA-Z\+,]+\s[a-z-]+}
+set pattern {[0-9]\s.+\s+[a-zA-Z\+,]+\s[a-z-]+}
 
 set aaa [regexp -all -inline -- $pattern $get_info]
 if {[regexp $::lic_profile $aaa]} {
@@ -47,7 +47,7 @@ if {[regexp $::lic_profile $aaa]} {
 ;# ===================================
 [0-9]+ --> 取得 ID --> 1
 \s+ --> 空一格
-[0-9]{4}-[0-9]{2}-[0-9]{2} --> 取得日期 --> 2026-05-24
+.+ --> 所有字元, 包含 Permanent & 2026-05-24
 \s+ --> 空白
 [a-zA-Z\+,]+ --> 取得 包含大小寫的英文字母, 和 (加號 +) 和 (逗號 ,) --> Essential,
 \s+ --> 空白
