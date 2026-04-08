96be26f5b71fc0287c70a1a46382abfd4b455de9
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-04-08 09:12:24 +0800

    Update 260408a . 判別 Cert Insta, EC & PKI1.tcl

--- book-0-regexp-常規表示式/260408 . 練習 Certificate/260408a . 判別 Cert Insta, EC & PKI1.tcl
+++ book-0-regexp-常規表示式/260408 . 練習 Certificate/260408a . 判別 Cert Insta, EC & PKI1.tcl
@@ -1,18 +1,10 @@
 
 Certificate file list:
 PKI 1.0
-  cas.pem
-  cert.pem
-  dev-id
-  key.pem
+--> set certlist_pki1 [list cas.pem cert.pem dev-id key.pem]
 
-PKI 2.0 files:
-  cas.pem
-  cert.pem
-  cert-id
-  key.pem
+PKI 2.0 Insta
+--> set certlist_pki2insta [list cas.pem cert.pem cert-id key.pem]
 
 PKI 2.0 EC (Self-Signed)
-  key.pem
-  operational.ca
-  operational.pem
+--> set certlist_pki2ec [list key.pem operational.ca operational.pem]
