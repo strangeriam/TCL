1e443b971d441299ebb131d75394120af56a9da9
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-04-08 08:31:04 +0800

    Create 260408a . 判別 Cert Insta, EC & PKI1.tcl

---
+++ book-0-regexp-常規表示式/260408 . 練習 Certificate/260408a . 判別 Cert Insta, EC & PKI1.tcl
@@ -0,0 +1,18 @@
+
+Certificate file list:
+PKI 1.0
+  cas.pem
+  cert.pem
+  dev-id
+  key.pem
+
+PKI 2.0 files:
+  cas.pem
+  cert.pem
+  cert-id
+  key.pem
+
+PKI 2.0 EC (Self-Signed)
+  key.pem
+  operational.ca
+  operational.pem
