50b8b992be0c9c2b9e27a102fdcb554cabc7e25a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-04-08 10:23:19 +0800

    Update 260408a . 判別 Cert Insta, EC & PKI1.tcl

--- book-0-regexp-常規表示式/260408 . 練習 Certificate/260408a . 判別 Cert Insta, EC & PKI1.tcl
+++ book-0-regexp-常規表示式/260408 . 練習 Certificate/260408a . 判別 Cert Insta, EC & PKI1.tcl
@@ -1,10 +1,22 @@
 
 Certificate file list:
-PKI 1.0
---> set certlist_pki1 [list cas.pem cert.pem dev-id key.pem]
+======================
+set certlist_pki1 [list cas.pem cert.pem dev-id key.pem] ;# --> PKI 1.0
+set certlist_pki2insta [list cas.pem cert.pem cert-id key.pem] ;# --> PKI 2.0 Insta
+set certlist_pki2ec [list key.pem operational.ca operational.pem] ;# --> PKI 2.0 EC (Self-Signed)
 
-PKI 2.0 Insta
---> set certlist_pki2insta [list cas.pem cert.pem cert-id key.pem]
+set mac E001A63FB633
 
-PKI 2.0 EC (Self-Signed)
---> set certlist_pki2ec [list key.pem operational.ca operational.pem]
+set listitem [glob -no -directory ${::ASPECTPATH}extapp/FTP_TFTP_AUTO/apcode_certs/$mac/ -tail *]
+set cert_num [llength $listitem]
+
+if { $cert_num == 3 } {
+    puts "Found Certificate EC Self-Signed"
+} elseif { $cert_num == 4 } {
+    puts "Found Certificate PKI 2.0 Insta or PKI 1.0"
+} else {
+    puts "Wrong Certificate file number ($cert_num)"
+}
+
+
+=============================
