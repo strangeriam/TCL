ec1076ccc05f30692d3a74fe4f12e32d043dad75
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-12 16:30:47 +0800

    Create 260512A . _f_Diag_TestB.tcl

---
+++ book-0-regexp-常規表示式/260512 練習/260512A . _f_Diag_TestB.tcl
@@ -0,0 +1,23 @@
+set get_info {
+acc_fs_usb rw
+16:23:39:757| 5m
+16:23:39:822| ##### (1970-01-01 02:08:24) Starting Diagnostic Test Case:[0m acc_fs_usb rw  
+16:23:39:822| 
+16:23:39:822| External USB: Read/Write test
+16:23:39:822| 
+16:23:39:893| Mounting /dev/sda1 to /mnt/usb_storage...OK.
+16:23:39:893| Testing /mnt/usb_storage, 10240 1K-blocks
+16:23:39:893| read/write file test...
+16:23:39:952| Read/Write Test /mnt/usb_storage: PASS
+16:23:42:030| ...........
+16:23:42:121| ...........
+16:23:42:141| remove test log...
+16:23:43:209| Stress Test /mnt/usb_storage: PASS
+16:23:43:280| 5m
+16:23:43:280| ###### (1970-01-01 02:08:28) Ending Diagnostic Test Case:[0m acc_fs_usb rw ............ PASS 
+16:23:43:280| 
+16:23:43:280| 0 minutes and 4 seconds elapsed.
+16:23:43:280| 
+16:23:43:280| LogFile: /mnt/log_rw/log/accdcp/unilog/acc_fs_usb+rw_19700101020824_PASS
+16:23:43:280| root@(none):/diagnostic# 
+}
