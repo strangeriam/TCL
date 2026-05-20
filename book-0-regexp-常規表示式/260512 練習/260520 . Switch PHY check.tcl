96bb568d39d282fcaa2c3941e192b283dc28ed2a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-20 10:22:10 +0800

    Create 260520 . Switch PHY check.tcl

---
+++ book-0-regexp-常規表示式/260512 練習/260520 . Switch PHY check.tcl
@@ -0,0 +1,31 @@
+if {[info exists ngList]} {
+	unset ngList
+}
+
+set pattern {\d+\s+\|\s+\d+ \|\s+\-\d+ \|\s+\d+\s+\|\s+\d+\s+\|\s+\d+\s+\|\s+FAIL}
+
+foreach line [regexp -all -inline $pattern $infile] {
+	set ngPort [string trim [lindex [split $line |] 1]]
+
+	append ngList "$ngPort "
+}
+
+
+;# ============================================
+set get_info {
+root@(none):/diagnostic# stdout_xform_temperature.sh
+Switching application is already running, doing nothing.
+
++----+---------------+---------+-------+----------+-----+-----+-----------+--------+
+| id |     name      | address | board | location | min | max | value('C) | result |
++----+---------------+---------+-------+----------+-----+-----+-----------+--------+
+|  0 | PHY_88E1780_1 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS |
+|  1 | PHY_88E1780_2 |     0x0 |     N |        N |   0 | 125 |        37 |   PASS |
+|  2 | PHY_88E1780_3 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS |
+|  3 | PHY_88E1780_4 |     0x0 |     N |        N |   0 | 125 |        32 |   PASS |
+|  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS |
+|  5 | PHY_88E1780_6 |     0x0 |     N |        N |   0 | 125 |        36 |   PASS |
+|  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS |
++----+---------------+---------+-------+----------+-----+-----+-----------+--------+
+root@(none):/diagnostic# 
+}
