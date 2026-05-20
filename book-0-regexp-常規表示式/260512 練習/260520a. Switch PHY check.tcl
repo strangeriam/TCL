a646c993a654a944cf6c879a8dd1c658d11d2e95
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-20 14:31:47 +0800

    Update 260520a. Switch PHY check.tcl

--- book-0-regexp-常規表示式/260512 練習/260520a. Switch PHY check.tcl
+++ book-0-regexp-常規表示式/260512 練習/260520a. Switch PHY check.tcl
@@ -72,12 +72,24 @@ root@(none):/diagnostic#
 }
 
 ;# ============================================
-if {[info exists ngList]} { unset ngList }
+set pattern {\d \|\s+[A-Z_0-9]+ \|\s+0x\d+ \|\s+N \|\s+N \|\s+[0-9-]+ \|\s+\d+ \|\s+\d+ \|\s+[PASSFIL]+}
 
+if {[info exists ngList]} { unset ngList }
 foreach line [regexp -all -inline $pattern $get_info] {
-	set ngPort [string trim [lindex [split $line |] 1]]
-	append ngList "$ngPort "
+	if { [lindex $line end-0] == "PASS" } {
+			puts "Check PASS --> $line"
+	} else {
+			puts "Check FAIL --> $line"
+			set ngPort [lindex $line 0]
+			append ngList "$ngPort "
+	}
 }
 
+if {[info exists ngList]} {
+	puts "NG PHY: $ngList"
+	return 0
+}
+
+return 1
 
 
