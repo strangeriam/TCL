81defb818fedab04fadefcbfe9d813d0da86c49a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-20 11:08:24 +0800

    Update 260520a. Switch PHY check.tcl

--- book-0-regexp-常規表示式/260512 練習/260520a. Switch PHY check.tcl
+++ book-0-regexp-常規表示式/260512 練習/260520a. Switch PHY check.tcl
@@ -5,18 +5,28 @@ regexp -all -inline $pattern $get_info
 set get_info {
 |  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS |
 }
-# 輸出: {|  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS}
+;# 輸出: {|  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS}
 
 set get_info {
 |  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS |
 }
-# 輸出: {|  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS}
+;# 輸出: {|  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS}
 
 
 set pattern {\|\s+\d \|\s+[A-Z_0-9]+ \|\s+0x\d+ \|\s+N \|\s+N \|\s+[0-9-]+ \|\s+\d+ \|\s+\d+ \|\s+PASS}
 foreach line [regexp -all -inline $pattern $get_info] {
 	puts "line: $line"
 }
+;# 輸出:
+line: |  0 | PHY_88E1780_1 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS
+line: |  1 | PHY_88E1780_2 |     0x0 |     N |        N |   0 | 125 |        37 |   PASS
+line: |  2 | PHY_88E1780_3 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS
+line: |  3 | PHY_88E1780_4 |     0x0 |     N |        N |   0 | 125 |        32 |   PASS
+line: |  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS
+line: |  5 | PHY_88E1780_6 |     0x0 |     N |        N |   0 | 125 |        36 |   PASS
+line: |  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS
+
+
 
 
 if {[info exists ngList]} { unset ngList }
