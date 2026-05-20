5eeb18617ec87546ac8ea4b02b3a40144c7e2de5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-20 13:39:19 +0800

    Update 260520a. Switch PHY check.tcl

--- book-0-regexp-常規表示式/260512 練習/260520a. Switch PHY check.tcl
+++ book-0-regexp-常規表示式/260512 練習/260520a. Switch PHY check.tcl
@@ -1,14 +1,14 @@
 
-set pattern {\|\s+\d \|\s+[A-Z_0-9]+ \|\s+0x\d+ \|\s+N \|\s+N \|\s+[0-9-]+ \|\s+\d+ \|\s+\d+ \|\s+PASS}
+set pattern {\d \|\s+[A-Z_0-9]+ \|\s+0x\d+ \|\s+N \|\s+N \|\s+[0-9-]+ \|\s+\d+ \|\s+\d+ \|\s+PASS}
 regexp -all -inline $pattern $get_info
 
 set get_info {
-|  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS |
+4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS |
 }
 ;# 輸出: {|  4 | PHY_88E1780_5 |     0x0 |     N |        N |   0 | 125 |        34 |   PASS}
 
 set get_info {
-|  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS |
+6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS |
 }
 ;# 輸出: {|  6 |       TMP75_1 |    0x48 |     N |        N | -40 | 125 |        32 |   PASS}
 
