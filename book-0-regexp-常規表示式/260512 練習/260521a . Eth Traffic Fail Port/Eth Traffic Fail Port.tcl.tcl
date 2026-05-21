5ba9e5b35a6763eac11745d52be67627ed9ee3be
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-21 13:39:17 +0800

    Update Eth Traffic Fail Port.tcl.tcl

--- book-0-regexp-常規表示式/260512 練習/260521a . Eth Traffic Fail Port/Eth Traffic Fail Port.tcl.tcl
+++ book-0-regexp-常規表示式/260512 練習/260521a . Eth Traffic Fail Port/Eth Traffic Fail Port.tcl.tcl
@@ -1,20 +1,15 @@
 
-set pattern {\d \|\s+[A-Z_0-9]+ \|\s+0x\d+ \|\s+N \|\s+N \|\s+[0-9-]+ \|\s+\d+ \|\s+\d+ \|\s+[PASSFIL]+}
-
+set spec 900
 if {[info exists ngList]} { unset ngList }
-foreach line [regexp -all -inline $pattern $get_info] {
-	if { [lindex $line end-0] == "PASS" } {
-			puts "Check PASS --> $line"
-	} else {
-			puts "Check FAIL --> $line"
+foreach line [regexp -all -inline {\d+\s+0/[ 0-9]+\s+} $get_info] {
+	if { [lindex $line end-0] < $spec } {
 			set ngPort [lindex $line 0]
 			append ngList "$ngPort "
 	}
 }
 
 if {[info exists ngList]} {
-	puts "NG PHY: $ngList"
+	puts "NG PORT: $ngList"
 	return 0
 }
 
-return 1
