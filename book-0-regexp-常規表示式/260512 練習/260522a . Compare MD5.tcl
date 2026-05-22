c7af88d22056adea1fdc0b6fca88b1f7443838fa
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-05-22 13:54:01 +0800

    Update and rename blank1 to 260522a . Compare MD5.tcl

---
+++ book-0-regexp-常規表示式/260512 練習/260522a . Compare MD5.tcl
@@ -0,0 +1,21 @@
+
+
+
+
+set get_info {
+show ucentral certificate status
+13:39:51:239| 
+13:39:51:239| TIP Certificate MD5 Checksum
+13:39:51:250| 
+13:39:51:250|   cas.pem   : dbdd18608c3cfe4d7f54e2a65f1b2d29
+13:39:51:266|   cert.pem  : e00b5b67335f567f91fcc4cc3f7f81fe
+13:39:51:282|   dev-id    : 4e28bba2ea2deefb58f524ce2bd77e4a
+13:39:51:335|   key.pem   : 8162c46bc37c064f23ef05fa1a9d02df
+13:39:51:335| Operational Certificate MD5 Checksum
+13:39:51:335| 
+13:39:51:335|   operational.pem: e00b5b67335f567f91fcc4cc3f7f81fe
+13:39:51:335| Operational Certificate Authority MD5 Checksum
+13:39:51:335| 
+13:39:51:335|   operational.ca: dbdd18608c3cfe4d7f54e2a65f1b2d29
+13:39:51:335| Console#
+}
