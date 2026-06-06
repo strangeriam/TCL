e239b2f5e70b9f6b13e08e28c26e8c7b7798cc76
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-06 08:04:39 +0800

    Create 260606 . _f_check_i2c.tcl

---
+++ book-0-regexp-常規表示式/260512 練習/260606 . _f_check_i2c.tcl
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+set get_info {
+root@(none):/diagnostic# acc_iai_i2c device
+
+##### (1970-01-01 00:18:13) Starting Diagnostic Test Case: acc_iai_i2c device  
+
+I2C Test: Scan I2C devices
+
++------------------+-----------+-------+----------+---------+--------+
+| all              |   Name    | Board | Location | Address | Result |
+|  :               +-----------+-------+----------+---------+--------+
+|  \_n             |           |       |          |         |        |
+|     :_i2c_0      |           |       |          |         |        |
+|     :  :_0x48    | TMP75_1   | CPU   | N        | 0x48    | PASS   |
+|     :  :_0x54    | ID_EEPROM | CPU   | N        | 0x54    | PASS   |
+|     :  :_0x66    | CPLD      | CPU   | N        | 0x66    | PASS   |
+|     :  \_0x76    | CLKgen    | CPU   | N        | 0x76    | PASS   |
+|     \_i2c_1      |           |       |          |         |        |
+|        \_0x11    | RoT       | CPI   | N        | 0x11    | FAIL   |
++------------------+-----------+-------+----------+---------+--------+
+
+###### (1970-01-01 00:18:14) Ending Diagnostic Test Case: acc_iai_i2c device ............ FAIL 
+
+Accton ErrorCode : 073
+0 minutes and 1 seconds elapsed.
+
+LogFile: /mnt/log_rw/log/accdcp/unilog/acc_iai_i2c+device_19700101001813_FAIL
+root@(none):/diagnostic# 
+}
