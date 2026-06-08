57259d77372d3094b1267d1ac1296761500baed0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-08 08:48:11 +0800

    Create 260608 . 48T _f_diagcode_hwinfo_write.tcl

---
+++ book-0-regexp-常規表示式/260512 練習/260608 . 48T _f_diagcode_hwinfo_write.tcl
@@ -0,0 +1,27 @@
+
+set itemlist [list  sn \
+                    mac \
+                    hw_version \
+                    manuf_date \
+                    proj_id \
+                    board_id \
+                    mod_id ]
+
+set valuelist [list EC2332000362 \
+                    90:2D:77:21:7C:60 \
+                    R0A \
+                    2026-06-06 \
+                    0x00000537 \
+                    0x00000000 \
+                    ECS4150-48T ]
+
+foreach item $itemlist value $valuelist {
+    
+
+}
+
+
+set get_info {
+
+
+}
