2e759e03cc579ea827c849bf7b91d5243790a5ed
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-08 09:01:58 +0800

    Update 260608 . 48T _f_diagcode_hwinfo_write.tcl

--- book-0-regexp-常規表示式/260512 練習/260608 . 48T _f_diagcode_hwinfo_write.tcl
+++ book-0-regexp-常規表示式/260512 練習/260608 . 48T _f_diagcode_hwinfo_write.tcl
@@ -22,6 +22,13 @@ foreach item $itemlist value $valuelist {
 
 
 set get_info {
-
-
+root@(none):/diagnostic# ideeprom_get_all.sh
+[       mac] 90:2d:77:21:7c:60 
+[        sn] EC2332000362 
+[hw_version] R0A 
+[manuf_date] 2026-06-06 
+[    mod_id] ECS4150-48T 
+[   proj_id] 0x00000537 
+[  board_id] 0x00000000 
+root@(none):/diagnostic# 
 }
