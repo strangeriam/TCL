445d96ea0de29e332e3df65d2fc34658b19f3e88
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-09 14:57:28 +0800

    Update 260606 . 48T _f_check_i2c.tcl

--- book-0-regexp-常規表示式/260512 練習/260606 . 48T _f_check_i2c.tcl
+++ book-0-regexp-常規表示式/260512 練習/260606 . 48T _f_check_i2c.tcl
@@ -18,30 +18,30 @@ if { [info exists faillist] } {
 
 
 set get_info {
-root@(none):/diagnostic# acc_iai_i2c device
-
-##### (1970-01-01 00:18:13) Starting Diagnostic Test Case: acc_iai_i2c device  
-
-I2C Test: Scan I2C devices
-
-+------------------+-----------+-------+----------+---------+--------+
-| all              |   Name    | Board | Location | Address | Result |
-|  :               +-----------+-------+----------+---------+--------+
-|  \_n             |           |       |          |         |        |
-|     :_i2c_0      |           |       |          |         |        |
-|     :  :_0x48    | TMP75_1   | CPU   | N        | 0x48    | PASS   |
-|     :  :_0x54    | ID_EEPROM | CPU   | N        | 0x54    | PASS   |
-|     :  :_0x66    | CPLD      | CPU   | N        | 0x66    | PASS   |
-|     :  \_0x76    | CLKgen    | CPU   | N        | 0x76    | PASS   |
-|     \_i2c_1      |           |       |          |         |        |
-|        \_0x11    | RoT       | CPI   | N        | 0x11    | FAIL   |
-+------------------+-----------+-------+----------+---------+--------+
-
-###### (1970-01-01 00:18:14) Ending Diagnostic Test Case: acc_iai_i2c device ............ FAIL 
-
-Accton ErrorCode : 073
-0 minutes and 1 seconds elapsed.
-
-LogFile: /mnt/log_rw/log/accdcp/unilog/acc_iai_i2c+device_19700101001813_FAIL
-root@(none):/diagnostic# 
+acc_iai_i2c device
+14:42:43:916| 5m
+14:42:43:995| ##### (1970-01-01 00:02:58) Starting Diagnostic Test Case:[0m acc_iai_i2c device  
+14:42:43:995| 
+14:42:43:995| I2C Test: Scan I2C devices
+14:42:43:995| 
+14:42:44:419| +------------------+-----------+-------+----------+---------+--------+
+14:42:44:419| | all              |   Name    | Board | Location | Address | Result |
+14:42:44:419| |  :               +-----------+-------+----------+---------+--------+
+14:42:44:419| |  \_n             |           |       |          |         |        |
+14:42:44:419| |     :_i2c_0      |           |       |          |         |        |
+14:42:44:419| |     :  :_0x48    | TMP75_1   | CPU   | N        | 0x48    | PASS   |
+14:42:44:419| |     :  :_0x54    | ID_EEPROM | CPU   | N        | 0x54    | PASS   |
+14:42:44:419| |     :  :_0x66    | CPLD      | CPU   | N        | 0x66    | PASS   |
+14:42:44:419| |     :  \_0x76    | CLKgen    | CPU   | N        | 0x76    | PASS   |
+14:42:44:419| |     \_i2c_1      |           |       |          |         |        |
+14:42:44:419| |        \_0x11    | RoT       | CPU   | N        | 0x11    | FAIL   |
+14:42:44:419| +------------------+-----------+-------+----------+---------+--------+
+14:42:44:419| 5m
+14:42:44:419| ###### (1970-01-01 00:02:58) Ending Diagnostic Test Case:[0m acc_iai_i2c device ............[1;31m FAIL [0m
+14:42:44:419| 
+14:42:44:419| Accton ErrorCode : 073
+14:42:44:419| 0 minutes and 0 seconds elapsed.
+14:42:44:419| 
+14:42:44:419| LogFile: /mnt/log_rw/log/accdcp/unilog/acc_iai_i2c+device_19700101000258_FAIL
+14:42:44:419| root@(none):/diagnostic# 
 }
