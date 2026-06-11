3931aab1e6e17055f6191ecb29c6253da9a18d81
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-11 16:33:07 +0800

    Update 260610 . 48T . _f_eth_traffic.tcl

--- book-0-regexp-常規表示式/260512 練習/260610 . 48T . _f_eth_traffic.tcl
+++ book-0-regexp-常規表示式/260512 練習/260610 . 48T . _f_eth_traffic.tcl
@@ -9,6 +9,22 @@ if { [info exists faillist]} {
   puts "faillist: $faillist"
 }
 
+;# 移除相同項目
+set list1 "AAA AAA BBB CCC CCC CCC"
+
+if {[info exists appendL]} { unset appendL }
+foreach item $list1 {
+	if {[regexp $item $list1]} {
+		append appendL "$item "
+		set list1 [string map "$item \"\"" $list1]
+	}
+}
+
+## 移除 appendL 最後的空白.
+set appendL [string trim $appendL]
+
+
+
 
 set get_info {
 15:43:03:089| ------ --------- ------------------
