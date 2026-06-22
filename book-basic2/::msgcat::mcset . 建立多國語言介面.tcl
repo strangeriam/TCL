5faf8d2e7b7ac0de2cd1a9a601b89f2fc7c0fae5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-22 09:55:32 +0800

    Update ::msgcat::mcset . 建立多國語言介面.tcl

--- book-basic2/::msgcat::mcset . 建立多國語言介面.tcl
+++ book-basic2/::msgcat::mcset . 建立多國語言介面.tcl
@@ -1,3 +1,10 @@
 Syntax --> ::msgcat::mcset locale src-string ?translate-string?
 
+en : 英文
+zh_tw : 繁體中文
+zh_cn : 簡體中文
+vi_vn : 越南文
+
+
+::msgcat::mcset vi_vn {Please Check LED if ON} {Vui lòng kiểm tra đèn LED nếu BẬT}
 
