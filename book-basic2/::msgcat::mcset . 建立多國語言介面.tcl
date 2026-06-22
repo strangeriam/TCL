ab5c47b62c34e9761a41855f777a53d208d05120
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-22 10:25:29 +0800

    Update ::msgcat::mcset . 建立多國語言介面.tcl

--- book-basic2/::msgcat::mcset . 建立多國語言介面.tcl
+++ book-basic2/::msgcat::mcset . 建立多國語言介面.tcl
@@ -1,10 +1,33 @@
+package require msgcat
+定義翻譯 --> ::msgcat::mcset zh_tw "Hello" "你好"
+目前系統語系 --> ::msgcat::mclocale zh_tw
+呼叫翻譯 --> puts [::msgcat::mc "Hello"] --> 輸出: 你好
+
+::msgcat::mcmset <-- 批次設定
+::msgcat::mcmset zh_tw {
+    "Hello"   "你好"
+    "Goodbye" "再見"
+    "Exit"    "離開"
+}
+
+
+語系檔
+a1. 副檔名為 .msg
+a2. 全部小寫
+a3. 檔名: en.msg , zh_tw.msg , vi_vn.msg , zh_cn
+
+
 Syntax --> ::msgcat::mcset locale src-string ?translate-string?
 
-en : 英文
-zh_tw : 繁體中文
-zh_cn : 簡體中文
-vi_vn : 越南文
+Parameter
+locale -->
+  . en : 英文
+  . zh_tw : 繁體中文
+  . zh_cn : 簡體中文
+  . vi_vn : 越南文
+src-string --> 預設英文字串
+translate-string --> (option) 對應的翻譯字串. 如省略此參數, Tcl 會直接使用 src-string 作為翻譯值.
 
-
+::msgcat::mcset en "Hello, World!"
 ::msgcat::mcset vi_vn {Please Check LED if ON} {Vui lòng kiểm tra đèn LED nếu BẬT}
-
+::msgcat::mcset zh_tw "Hello, World!" "哈囉，世界！"
