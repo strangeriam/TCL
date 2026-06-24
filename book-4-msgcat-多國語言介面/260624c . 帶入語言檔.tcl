8d3a21f142b09934320b7e03fd49d0522d942ae8
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-06-24 14:55:38 +0800

    Update 260624c . 帶入語言檔.tcl

--- book-4-msgcat-多國語言介面/260624c . 帶入語言檔.tcl
+++ book-4-msgcat-多國語言介面/260624c . 帶入語言檔.tcl
@@ -1,22 +1,21 @@
-proc _f_language_detect_260624 {} {
-	package require msgcat
+package require msgcat
 
-	if { [string equal -nocase [_f_vini_profilerd DUTINFO LANGUAGE] "vn"] } {
+set ::lang tw
+
+if { [string equal -nocase $::lang "vn"] } {
 		source ${::ASPECTPATH}/locale/vi_vn.msg
 		::msgcat::mclocale vi_vn
-	} elseif { [string equal -nocase [_f_vini_profilerd DUTINFO LANGUAGE] "tw"] } {
+} elseif { [string equal -nocase $::lang "tw"] } {
 		source ${::ASPECTPATH}/locale/zh_tw.msg
 		::msgcat::mclocale zh_tw
-	} else {
-	}
 }
 
+puts "[::msgcat::mc "NOT FOUND"] APC"
+;# 輸出:
+找不到 APC
+
+==================================================
 ;# 語言檔內容
 encoding system utf-8
 ::msgcat::mcset zh_tw {SHORT PRESS} {短按}
 ::msgcat::mcset zh_tw {NOT FOUND} {找不到}
-
-==================================================
-set msg "[::msgcat::mc "NOT FOUND"] APC"
-;# 輸出:
-找不到 APC
