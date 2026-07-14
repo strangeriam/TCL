8f9eb09bd0363ce37f00ba50264f1684d5809271
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-14 11:51:50 +0800

    Create 260714a . check MAC & IP.tcl

---
+++ book-0-regexp-常規表示式/260714 練習/260714a . check MAC & IP.tcl
@@ -0,0 +1,21 @@
+set get_info {
+11:46:24:243| Console#show ip interface
+11:46:24:285| 
+11:46:24:285| VLAN 1 is Administrative Up - Link Up
+11:46:24:328| 
+11:46:24:328|   Address is 00-E0-0C-00-00-FD
+11:46:24:328| 
+11:46:24:328|   Index: 1001, MTU: 1500
+11:46:24:328| 
+11:46:24:328|   Address Mode is User specified
+11:46:24:328| 
+11:46:24:328|   IP Address: 192.168.1.11 Mask: 255.255.255.0
+11:46:24:328| 
+11:46:24:328|   Proxy ARP is disabled
+11:46:24:328| 
+11:46:24:328|   DHCP Client Vendor Class ID (text): OpenLAN
+11:46:24:328| 
+11:46:24:328|   DHCP Relay Server: 
+11:46:24:328| 
+11:46:24:328| Console#
+}
