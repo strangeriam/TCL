b2d2914279b1ca9f782dce0888a6cdae0bc6e294
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-15 10:16:07 +0800

    Update 260715a . Switch 檢查 Port License.tcl

--- book-0-regexp-常規表示式/260714 練習/260715a . Switch 檢查 Port License.tcl
+++ book-0-regexp-常規表示式/260714 練習/260715a . Switch 檢查 Port License.tcl
@@ -2,4 +2,35 @@
 
 
 set get_info {
+show interfaces brief
+10:13:16:265| 
+10:13:16:265| Interface Name              Status    PVID Pri Speed/Duplex  Type         Trunk
+10:13:16:265| 
+10:13:16:265| --------- ----------------- --------- ---- --- ------------- ------------ -----
+10:13:16:265| 
+10:13:16:265| Eth 1/ 1                    Up           1   0 Auto-1000full 1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 2                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 3                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 4                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 5                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 6                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 7                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 8                    Down         1   0 Auto          1000BASE-T   None 
+10:13:16:265| 
+10:13:16:265| Eth 1/ 9                    Down         1   0 10Gfull       10GBASE SFP+ None 
+10:13:16:265| 
+10:13:16:265| Eth 1/10                    Down         1   0 10Gfull       10GBASE SFP+ None 
+10:13:16:265| 
+10:13:16:265| Eth 1/11                    Down         1   0 10Gfull       10GBASE SFP+ None 
+10:13:16:265| 
+10:13:16:265| Eth 1/12                    Down         1   0 10Gfull       10GBASE SFP+ None 
+10:13:16:265| 
+10:13:16:265| Console#
 }
