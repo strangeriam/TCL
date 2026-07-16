f11a2a612126c18bdac7cf4bf2da872fc77c87fc
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-16 09:50:15 +0800

    Update 260716 . 練習 1.tcl

--- book-basic1/260716 . switch/練習/260716 . 練習 1.tcl
+++ book-basic1/260716 . switch/練習/260716 . 練習 1.tcl
@@ -1,3 +1,17 @@
-set ::certlist [list cas.pem cert.pem dev-id key.pem operational.ca operational.pem]
+set listitem [list cas.pem cert.pem]
 
+foreach fname $listitem {
+    switch $fname {
+          cas.pem {
+                    set ::md5_[string map {. _} $fname] AAA
+                    set ::length_[string map {. _} $fname] bbb
+          }
+          cert.pem {
+                    set ::md5_[string map {. _} $fname] BBB
+                    set ::length_[string map {. _} $fname] bbb
+          }
+    }
+}
 
+puts "::md5_cas_pem : $::md5_cas_pem"
+puts "::md5_cert_pem : $::md5_cert_pem"
