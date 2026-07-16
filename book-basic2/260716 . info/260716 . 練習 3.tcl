67ef6a723394c1b4178da8ee889ed63f322da3c7
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-16 10:20:17 +0800

    Update 260716 . 練習 3.tcl

--- book-basic2/260716 . info/260716 . 練習 3.tcl
+++ book-basic2/260716 . info/260716 . 練習 3.tcl
@@ -2,4 +2,11 @@
 set listitem [list cas.pem cert.pem]
 
 foreach fname $listitem {
+		set fn [string map {. _} $fname]
+		set ::md5_$fn AAA
+		set ::length_$fn aaa
+
+		if { ![info exist ::md5_$fn] || ![info exist ::length_$fn] } {
+			  puts "Unknown CERT !!!"
+		}
 }
