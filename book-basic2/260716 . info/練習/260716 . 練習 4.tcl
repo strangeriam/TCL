52ab8d7f6362b3927502f8993c2958abd319c32c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-16 10:47:54 +0800

    Update 260716 . 練習 4.tcl

--- book-basic2/260716 . info/練習/260716 . 練習 4.tcl
+++ book-basic2/260716 . info/練習/260716 . 練習 4.tcl
@@ -2,10 +2,14 @@
 set ::md5_cas_pem AAA
 set ::length_cas_pem bbb
 
-set listitem [list ::md5_cas_pem ::length_cas_pem]
-
-foreach item $listitem {
-    if {![info exists ::md5_cas_pem]} {
+if {![info exists ::md5_cas_pem]} {
         puts "ERROR"
-    }
 }
+
+輸出:
+
+if {![info exists ::md5_key_id]} {
+        puts "ERROR"
+}
+
+輸出: ERROR
