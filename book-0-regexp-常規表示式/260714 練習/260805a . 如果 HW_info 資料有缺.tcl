23d1c70e2c4b8186f194a319bbf1d4ef22b92da9
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-08-05 15:10:34 +0800

    Update 260805a . 如果 HW_info 資料有缺.tcl

--- book-0-regexp-常規表示式/260714 練習/260805a . 如果 HW_info 資料有缺.tcl
+++ book-0-regexp-常規表示式/260714 練習/260805a . 如果 HW_info 資料有缺.tcl
@@ -56,14 +56,14 @@ set get_info {
 printsysinfo
 14:46:41:481| UC_MGR_GetSysInfo 000000023fe00000 ok
 14:46:41:531| ----------------------------------------
-14:46:41:531| Project ID           =0x0000020b
-14:46:41:531| Board ID             =0x00000004
+14:46:41:531| Project ID           =
+14:46:41:531| Board ID             =
 14:46:41:531| Bootloader Version   =
-14:46:41:531| Hardware Version     =R01
-14:46:41:531| Serial Number        =EC2332000362
-14:46:41:531| Mac Address          =5C:17:83:4A:A4:A0
+14:46:41:531| Hardware Version     =
+14:46:41:531| Serial Number        =
+14:46:41:531| Mac Address          =
 14:46:41:531| Manufacture Date     =2026-08-05
-14:46:41:531| Model Number         =ECS4150-28F
+14:46:41:531| Model Number         =
 14:46:41:531| Service Tag          =CONFIG_SERVICE_TAG
 14:46:41:531| ----------------------------------------
 14:46:41:531| Marvell>>
