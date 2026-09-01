9aee3249b2f0d9a629fbdef9695916ff6338190b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-09-01 16:38:04 +0800

    Create 260901a . PKI2-EC 憑證檢查.tcl

---
+++ book-0-regexp-常規表示式/260714 練習/260901a . PKI2-EC 憑證檢查.tcl
@@ -0,0 +1,21 @@
+
+
+
+
+set get_info {
+Console#show ucentral certificate status
+16:12:59:406| 
+16:12:59:406| TIP Certificate MD5 Checksum
+16:12:59:609| 
+16:12:59:609|   cas.pem   : NONE
+16:12:59:609|   cert.pem  : e07a20ced13f61bd2ef744eab1bdccfb
+16:12:59:609|   dev-id    : NONE
+16:12:59:609|   key.pem   : 28f6bf3a3444a7923588f08224a0fc61
+16:12:59:609| Operational Certificate MD5 Checksum
+16:12:59:609| 
+16:12:59:609|   operational.pem: e07a20ced13f61bd2ef744eab1bdccfb
+16:12:59:609| Operational Certificate Authority MD5 Checksum
+16:12:59:609| 
+16:12:59:609|   operational.ca: e32e46fc190abe06f1046552d25c2418
+16:12:59:609| Console#
+}
