633552f2b37d72a130ee7667930944381710d086
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-09-02 09:59:12 +0800

    Update 260902a . 12P . pki2 cloud link 檢查.tcl

--- book-0-regexp-常規表示式/260714 練習/260902a . 12P . pki2 cloud link 檢查.tcl
+++ book-0-regexp-常規表示式/260714 練習/260902a . 12P . pki2 cloud link 檢查.tcl
@@ -1,9 +1,22 @@
 
+;# 準備
 set ec_pki "00e00c2435fe.redirector.my-openwifi.cloud"
 
+;# ==============================================================
+;# Step 3: 判斷結果 --> ec_pki.
+regexp -all $ec_pki $tmp
+
+
+;# ==============================================================
+;# Step 2: 拿掉 跳行 \n.
+set tmp [regsub -all -line \n $tmp ""]
+
+輸出:
+show ucentral redirectorUcentral Redirector URL by Operational cert: 00e00c2435fe.redirector.my-openwifi.cloudConsole#
+
+;# ==============================================================
 ;# Step 1: 拿掉 log 產生的時間, eg. "08:20:09:759|"
-set get_info2 [regsub -all -line {\d+:\d+:\d+:\d+\| } $get_info ""]
-puts $get_info2
+set tmp [regsub -all -line {\d+:\d+:\d+:\d+\| } $get_info ""]
 
 輸出:
 show ucentral redirector
