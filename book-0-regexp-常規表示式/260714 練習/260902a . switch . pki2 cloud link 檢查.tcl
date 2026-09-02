7ff2f12210e54299932614350f3c3f5c2b0b2951
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-09-02 08:23:27 +0800

    Update 260902a . switch . pki2 cloud link 檢查.tcl

--- book-0-regexp-常規表示式/260714 練習/260902a . switch . pki2 cloud link 檢查.tcl
+++ book-0-regexp-常規表示式/260714 練習/260902a . switch . pki2 cloud link 檢查.tcl
@@ -1,1 +1,11 @@
-sfsfs
+
+
+set get_info {
+show ucentral redirector
+08:20:09:759| 
+08:20:09:759| Ucentral Redirector URL by Operational cert: 00e00c2435fe.redirector.my-openwifi
+08:20:09:759| 
+08:20:09:759| .cloud
+08:20:09:759| 
+08:20:09:759| Console#
+}
