cd67074548908d0ce7a3c1b4fd09a0b05dcc9c76
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-09-03 10:09:46 +0800

    Update 260903a . 12P . pki2 TE cloud link 檢查 . byKiro.tcl

--- book-0-regexp-常規表示式/260714 練習/260903a . 12P . pki2 TE cloud link 檢查 . byKiro.tcl
+++ book-0-regexp-常規表示式/260714 練習/260903a . 12P . pki2 TE cloud link 檢查 . byKiro.tcl
@@ -1,17 +1,35 @@
 ;# 目標: 取得正確字串 --> 00e00c2435fe.redirector.my-openwifi.cloud
 
-;# Step 1:
-set mac "00E00C2435FE"
-set ec_pki "[string tolow $mac].redirector.my-openwifi.cloud"
 
-;# 輸出:
-00e00c2435fe.redirector.my-openwifi.cloud
+;# ====================================
+;# Step 4: 統一小寫.
+set tmp [string tolower $tmp]
+
+輸出:
+showucentralredirectorucentralredirectorurlbyoperationalcert:00e00c2435fe.redirector.my-openwifi.cloudconsole#
+
+;# Step 5: 用 string first 做字面比對 (避免 . 被當萬用字元)
+if { [string first $ec_pki $tmp] >= 0 } {
+    puts "Check ec_pki \"$ec_pki\" ,PASS"
+}
+
+輸出:
+Check ec_pki "00e00c2435fe.redirector.my-openwifi.cloud" ,PASS
+
+
+;# ====================================
+;# Step 3: 所有換行/空白.
+set tmp [regsub -all {[\r\n[:space:]]} $tmp ""]
+
+輸出:
+showucentralredirectorUcentralRedirectorURLbyOperationalcert:00e00c2435fe.redirector.my-openwifi.cloudConsole#
+
 
 ;# ====================================
 ;# Step 2: 移除時間戳前綴.
 set tmp [regsub -all -line {\d+:\d+:\d+:\d+\| } $get_info ""]
 
-;# 輸出:
+輸出:
 show ucentral redirector
 
 Ucentral Redirector URL by Operational cert: 00e00c2435fe.redirector.my-openwifi
@@ -20,27 +38,13 @@ Ucentral Redirector URL by Operational cert: 00e00c2435fe.redirector.my-openwifi
 
 Console#
 
-;# ====================================
-;# Step 3: 所有換行/空白.
-set tmp [regsub -all {[\r\n[:space:]]} $tmp ""]
 
-;# 輸出
-showucentralredirectorUcentralRedirectorURLbyOperationalcert:00e00c2435fe.redirector.my-openwifi.cloudConsole#
+;# Step 1:
+set mac "00E00C2435FE"
+set ec_pki "[string tolow $mac].redirector.my-openwifi.cloud"
 
-;# ====================================
-;# Step 4: 統一小寫.
-set tmp [string tolower $tmp]
-
-;# 輸出
-showucentralredirectorucentralredirectorurlbyoperationalcert:00e00c2435fe.redirector.my-openwifi.cloudconsole#
-
-;# Step 5: 用 string first 做字面比對 (避免 . 被當萬用字元)
-if { [string first $ec_pki $tmp] >= 0 } {
-    puts "Check ec_pki \"$ec_pki\" ,PASS"
-}
-
-;# 輸出:
-Check ec_pki "00e00c2435fe.redirector.my-openwifi.cloud" ,PASS
+輸出:
+00e00c2435fe.redirector.my-openwifi.cloud
 
 
 ;# ===============================================================
