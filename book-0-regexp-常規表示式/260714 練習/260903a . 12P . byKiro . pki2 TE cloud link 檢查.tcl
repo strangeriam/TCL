c011c8f0e314939397374e11947637e55d35dbb5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-09-03 09:29:30 +0800

    Create 260903a . 12P . byKiro . pki2 TE cloud link 檢查.tcl

---
+++ book-0-regexp-常規表示式/260714 練習/260903a . 12P . byKiro . pki2 TE cloud link 檢查.tcl
@@ -0,0 +1,15 @@
+00e00c2435fe.redirector.my-openwifi.cloud
+
+set ec_pki "[string tolow [_f_vini_profilerd SFIS MAC]].redirector.my-openwifi.cloud"
+...
+;# 移除時間戳前綴 + 所有換行/空白, 統一小寫
+set tmp [_f_getconsole]
+set tmp [regsub -all -line {\d+:\d+:\d+:\d+\| } $tmp ""]
+set tmp [regsub -all {[\r\n[:space:]]} $tmp ""]
+set tmp [string tolower $tmp]
+
+;# 用 string first 做字面比對 (避免 . 被當萬用字元)
+if { [string first $ec_pki $tmp] >= 0 } {
+    _f_termmsg_V2 "Check ec_pki \"$ec_pki\" ,PASS" "" = -nodisplaytime
+    break
+}
