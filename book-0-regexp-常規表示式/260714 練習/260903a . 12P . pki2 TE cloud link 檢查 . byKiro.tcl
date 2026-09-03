35727cca64e5fc98263da46633f675f761fd4b60
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-09-03 09:39:31 +0800

    Update 260903a . 12P . pki2 TE cloud link 檢查 . byKiro.tcl

--- book-0-regexp-常規表示式/260714 練習/260903a . 12P . pki2 TE cloud link 檢查 . byKiro.tcl
+++ book-0-regexp-常規表示式/260714 練習/260903a . 12P . pki2 TE cloud link 檢查 . byKiro.tcl
@@ -14,6 +14,29 @@ if { [string first $ec_pki $tmp] >= 0 } {
     break
 }
 
+;# Step 1:
+set mac "00E00C2435FE"
+set ec_pki "[string tolow $mac].redirector.my-openwifi.cloud"
+
+;# 輸出:
+;# ===============================================================================
+00e00c2435fe.redirector.my-openwifi.cloud
+;# ===============================================================================
+
+;# Step 2: 移除時間格式.
+set tmp [regsub -all -line {\d+:\d+:\d+:\d+\| } $get_info ""]
+
+;# 輸出:
+;# ===============================================================================
+show ucentral redirector
+
+Ucentral Redirector URL by Operational cert: 00e00c2435fe.redirector.my-openwifi
+
+.cloud
+
+Console#
+;# ===============================================================================
+
 
 
 ;# ===============================================================
